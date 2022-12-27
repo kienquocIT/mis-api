f6fecb699707141485d741174698e6f965729a7c
namtt <namtt@mtsolution.com.vn>
2022-12-27 20:52:28 +0700

    [development/ms2] update apps/core/account/views.py (set_password user)

--- apps/core/account/views.py
+++ apps/core/account/views.py
@@ -34,9 +34,12 @@ class UserList(
     @swagger_auto_schema(operation_summary='Create New User', request_body=UserCreateSerializer)
     def post(self, request, *args, **kwargs):
         data = request.data
+        password = data['password']
         user = UserCreateSerializer(data=data)
         if user.is_valid():
-            user.save(tenant_current=request.user.tenant_current)
+            obj = user.save(tenant_current=request.user.tenant_current)
+            obj.set_password(password)
+            obj.save()
             return ResponseController.success_200(
                 data=user.data,
                 key_data='result',
