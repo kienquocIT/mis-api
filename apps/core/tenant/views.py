28440520cf70dbdac7158c956e6c848f488f08f6
haind <nguyenduonghai07@gmail.com>
2023-01-09 09:24:51 +0700

    [ms-78] update api

--- apps/core/tenant/views.py
+++ apps/core/tenant/views.py
@@ -1,16 +1,16 @@
 from drf_yasg.utils import swagger_auto_schema
 from apps.core.company.models import Company
 from apps.shared import mask_view, BaseListMixin, BaseCreateMixin
-from apps.core.tenant.serializers import (TenantDetailSerializer,)
+from apps.core.tenant.serializers import (TenantInformationSerializer,)
 
 
-class TenantDetail(BaseListMixin, BaseCreateMixin):
+class TenantInformation(BaseListMixin, BaseCreateMixin):
     queryset = Company.object_normal.all()
-    serializer_list = TenantDetailSerializer
+    serializer_list = TenantInformationSerializer
 
     @swagger_auto_schema(
-        operation_summary="Tenant Detail",
-        operation_description="Tenant Detail",
+        operation_summary="Tenant Information",
+        operation_description="Tenant Information",
     )
     @mask_view(login_require=True, auth_require=True, code_perm='')
     def get(self, request, *args, **kwargs):
