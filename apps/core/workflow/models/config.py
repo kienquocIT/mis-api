44debb1369538d30930024c60c5b30b5f9699c7a
quannm0105 <woongnguyen.15@gmail.com>
2023-02-13 15:56:42 +0700

    [feature/ms-91][base workflow]

--- apps/core/workflow/models/config.py
+++ apps/core/workflow/models/config.py
@@ -10,6 +10,14 @@ class Workflow(TenantCoreModel):
         null=True,
         help_text="code of application in base"
     )
+    is_active = models.BooleanField(
+        verbose_name='active status',
+        default=True
+    )
+    is_multi_company = models.BooleanField(
+        verbose_name='Multi company',
+        default=False
+    )
 
     class Meta:
         verbose_name = 'Workflow'
@@ -18,3 +26,31 @@ class Workflow(TenantCoreModel):
         default_permissions = ()
         permissions = ()
 
+
+class Zone(TenantCoreModel):
+    workflow = models.ForeignKey(
+        'workflow.Workflow',
+        on_delete=models.CASCADE,
+        related_name="zone_workflow",
+        null=False
+    )
+    name = models.TextField(
+        verbose_name="name",
+        required=True
+    )
+    remark = models.TextField(
+        verbose_name="description",
+        required=False
+    )
+    zone_field = JSONField(
+        verbose_name="zone field",
+        required=True,
+        default=[]
+    )
+
+    class Meta:
+        verbose_name = 'Zone in workflow'
+        verbose_name_plural = 'Zone in workflow'
+        ordering = ('-date_created',)
+        default_permissions = ()
+        permissions = ()
