a67143fe3f08ddd5f3cfbd78871946e33cbc2edc
haind <nguyenduonghai07@gmail.com>
2023-03-02 15:13:33 +0700

    update

--- apps/sale/saledata/urls.py
+++ apps/sale/saledata/urls.py
@@ -2,30 +2,24 @@ from django.urls import path, include
 
 from apps.sale.saledata.views.accounts import (
     SalutationList, InterestsList, AccountTypeList, IndustryList,
-
-    ContactList, ContactDraftList, ContactDetail, ContactDraftDetail,
+    ContactList, ContactDetail,
     ContactListNotMapAccount,
-
     AccountList, AccountDetail, EmployeeMapAccountList
 )
 
 urlpatterns = [
     path('salurations', SalutationList.as_view(), name='SalutationList'),
-    path('saluration/<str: pk>', SalutationList.as_view(), name='SalutationList'),
-    path('contact/interest/list', InterestsList.as_view(), name='InterestsList'),
-    path('account/accounttype/list', AccountTypeList.as_view(), name='AccountTypeList'),
-    path('account/industry/list', IndustryList.as_view(), name='IndustryList'),
+    path('interests', InterestsList.as_view(), name='InterestsList'),
+    path('accounttypes', AccountTypeList.as_view(), name='AccountTypeList'),
+    path('industries;', IndustryList.as_view(), name='IndustryList'),
 
     # contact
-    path('list', ContactList.as_view(), name='ContactList'),
-    path('list/<str:pk>', ContactDetail.as_view(), name='ContactDetail'),
+    path('contacts', ContactList.as_view(), name='ContactList'),
+    path('contact/<str:pk>', ContactDetail.as_view(), name='ContactDetail'),
     path('listnotmapaccount', ContactListNotMapAccount.as_view(), name='ContactListNotMapAccount'),
 
-    path('draft/list', ContactDraftList.as_view(), name='ContactDraftList'),
-    path('draft/list/<str:pk>', ContactDraftDetail.as_view(), name='ContactDraftDetail'),
-
     # account
-    path('list', AccountList.as_view(), name='AccountList'),
-    path('list/<str:pk>', AccountDetail.as_view(), name='AccountDetail'),
+    path('accounts', AccountList.as_view(), name='AccountList'),
+    path('account/<str:pk>', AccountDetail.as_view(), name='AccountDetail'),
     path('employee_map_account_list', EmployeeMapAccountList.as_view(), name='EmployeeMapAccountList'),
 ]
