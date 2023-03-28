6a8efdcd96e5a69a1485c42f42fdfe5e085d2fed
trieunt <trieunt@mtsolution.com.vn>
2023-03-29 00:19:02 +0700

    [MASTER] Fix merge error from branch MS-153 --> override my code test.

--- apps/core/auths/tests.py
+++ apps/core/auths/tests.py
@@ -1,51 +1,117 @@
 from django.urls import reverse
-from rest_framework.test import APITestCase
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from apps.core.provisioning.tests import TestCaseProvisioning
+from apps.shared import AdvanceTestCase
 
 
-# Create your tests here.
+class TestCaseAuth(AdvanceTestCase):
+    def setUp(self) -> None:
+        self.maxDiff = None
+        self.client = APIClient()
 
-
-class AuthTestCase(APITestCase):
-    def call_login(self):
-        new_tenant = {
-            "tenant_data": {
-                "title": "Cong ty Tai Chinh F8",
-                "code": "F88",
-                "sub_domain": "F88",
-                "representative_fullname": "string",
-                "representative_phone_number": "string",
-                "auto_create_company": True,
-                "company_quality_max": 5,
-                "user_request_created": {
-                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                    "full_name": "string",
-                    "email": "string",
-                    "phone": "string"
-                },
-                "plan": {}
-            },
-            "user_data": {
-                "first_name": "Nam",
-                "last_name": "Trinh Tuan",
-                "email": "namt9@gmail.com",
-                "phone": "01976741334",
-                "username": "admin",
-                "password": "111111"
-            },
-            "create_admin": True,
-            "create_employee": False,
-            "plan_data": {}
-        }
-        url_tenant = reverse('NewTenant')
-        a = self.client.post(url_tenant, new_tenant, format='json')
-
-        # login
-        url_login = reverse('AuthLogin')
+    def test_login(self):
+        new_tenant = self.call_another(
+            TestCaseProvisioning,
+            'test_new_tenant'
+        )
+        url = reverse('AuthLogin')
         data = {
-            "tenant_code": "F88",
-            "username": "admin",
-            "password": "111111"
+            'tenant_code': new_tenant['code'],
+            'username': 'queptl',
+            'password': 'queptl@1234',
         }
-        response_login = self.client.post(url_login, data, format='json')
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response_login.data['result']['token']['access_token'])
-        return response_login
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertResponseList(
+            response,
+            status_code=status.HTTP_200_OK,
+            key_required=['result', 'status'],
+            all_key=['result', 'status'],
+            all_key_from=response.data,
+            type_match={
+                'result': dict,
+                'status': int,
+            }
+        )
+        self.assertCountEqual(
+            ['id', 'first_name', 'last_name', 'username_auth', 'username', 'email', 'last_login', 'is_admin_tenant',
+             'language', 'tenant_current', 'company_current', 'space_current',
+             'employee_current', 'companies', 'token',
+             ],
+            list(response.data['result'].keys()),
+            check_sum_second=False,
+        )
+        self.assertCountEqual(
+            ['access_token', 'refresh_token'],
+            list(response.data['result']['token'].keys())
+        )
+        return response.data['result']
+
+    def test_my_profile(self):
+        login_data = self.test_login()
+        self.authenticated(login_data)
+        url = reverse('MyProfile')
+        response = self.client.get(url, format='json')
+        self.assertResponseList(
+            response,
+            status_code=status.HTTP_200_OK,
+            key_required=['result', 'status'],
+            all_key=[
+                'id', 'first_name', 'last_name', 'username_auth', 'username', 'email', 'last_login', 'is_admin_tenant',
+                'language', 'tenant_current', 'company_current', 'space_current', 'employee_current', 'companies',
+            ],
+            all_key_from=response.data['result']['data'],
+            type_match={
+                'result': dict,
+                'status': int,
+            }
+        )
+        return response.data['result']['data']
+
+    def test_alive_check(self):
+        login_data = self.test_login()
+        self.authenticated(login_data)
+        url = reverse('AliveCheck')
+        response = self.client.get(url, format='json')
+        self.assertResponseList(
+            response,
+            status_code=status.HTTP_200_OK,
+            key_required=['result', 'status'],
+            all_key=['state'],
+            all_key_from=response.data['result'],
+            type_match={
+                'result': dict,
+                'status': int,
+            }
+        )
+        self.assertEqual(response.data['result']['state'], 'You are still alive.')
+        return response.data
+
+    def test_refresh_token(self):
+        login_data = self.test_login()
+        self.authenticated(login_data)
+        url = reverse('AuthRefreshLogin')
+        data = {'refresh': login_data['token']['refresh_token']}
+        response = self.client.post(url, data, format='json')
+        self.assertResponseList(
+            response,
+            status_code=status.HTTP_200_OK,
+            key_required=['result', 'status'],
+            all_key=['access_token', ],
+            all_key_from=response.data['result'],
+            type_match={
+                'result': dict,
+                'status': int,
+            }
+        )
+        return response.data['result']
+
+    def test_switch_company(self):
+        # call login
+        # call add to another company, in tenant.
+        # call main VIEW test
+        return True
+
