1afe000c4eb3ccfb7958537c8891927b71c5f630
trieunt <trieunt@mtsolution.com.vn>
2023-03-28 22:30:36 +0700

    [MASTER] Clean migrations and normalized default data of base.

--- apps/sharedapp/data/base.py
+++ apps/sharedapp/data/base.py
@@ -3,6 +3,7 @@ __all__ = [
     'Application_data',
     'PlanApplication_data',
     'PermissionApplication_data',
+    'ApplicationProperty_data'
 ]
 
 SubscriptionPlan_data = {
@@ -28,70 +29,97 @@ Application_data = {
     "2204fbed-7c93-462c-9b7d-85e0a8574094": {
         "title": "Contract",
         "code": "contract",
+        "app_label": ''
     },
     "2d83c8bb-cf77-452f-af0b-fcab49b83527": {
         "title": "Sale Order",
         "code": "sale_order",
+        "app_label": ''
     },
     "48e995a6-cd8c-4aef-9a45-6b0ae9c500b2": {
         "title": "Task",
         "code": "task",
+        "app_label": 'timesheet'
     },
     "4cdaabcc-09ae-4c13-bb4e-c606eb335b11": {
         "title": "Role",
         "code": "role",
+        "app_label": 'hr'
     },
     "4e4c6574-e068-4a0d-94f2-4b89626676b4": {
         "title": "Over Time",
         "code": "OT",
+        "app_label": ''
     },
     "50348927-2c4f-4023-b638-445469c66953": {
         "title": "Employee",
         "code": "employee",
+        "app_label": 'hr'
     },
     "5b67fb7f-03e0-44ee-9df8-8cb0aae16d8f": {
         "title": "Document Management",
         "code": "edms",
+        "app_label": ''
     },
     "7d69e8df-7ff4-4230-897d-95e4521f055c": {
         "title": "Asset Management",
         "code": "eam",
+        "app_label": ''
     },
     "83de3e2e-d4d1-4864-ad48-fddd28da01df": {
         "title": "Annual Leave",
         "code": "leave",
+        "app_label": ''
     },
     "888c533e-4e87-480e-884b-6a23026e6c38": {
         "title": "Advance",
         "code": "advance",
+        "app_label": ''
     },
     "918de2cf-7416-4a24-85d1-100403fc025b": {
         "title": "Quotation",
         "code": "quotation",
+        "app_label": ''
     },
     "9b777adc-c8ce-4d43-a1d3-02f0434243ff": {
         "title": "Transportation",
         "code": "transportation",
+        "app_label": ''
     },
     "acf7c5ad-0f4b-49e2-9fbd-a6daa032efb6": {
         "title": "Payment",
         "code": "payment",
+        "app_label": ''
     },
     "b0a2d464-3dc9-450e-96cd-76db5c4b3d58": {
         "title": "Booking Room",
         "code": "booking",
+        "app_label": ''
     },
     "c5c22111-bda4-4935-8ea5-632e36554a5b": {
         "title": "Opportunity",
         "code": "opportunity",
+        "app_label": ''
     },
     "e17b9123-8002-4c9b-921b-7722c3c9e3a5": {
         "title": "Group",
         "code": "group",
+        "app_label": 'hr'
     },
     "f18c9cb9-6bbc-4efc-af10-4f5e244ad66e": {
         "title": "Contract Lock",
         "code": "lock",
+        "app_label": ''
+    },
+    "828b785a-8f57-4a03-9f90-e0edf96560d7": {
+        "title": "Contact",
+        "code": "contact",
+        'app_label': 'saledata',
+    },
+    "4e48c863-861b-475a-aa5e-97a4ed26f294": {
+        "title": "Account",
+        "code": "account",
+        'app_label': 'saledata',
     },
 }
 
@@ -166,4 +194,173 @@ PlanApplication_data = {
     }
 }
 
-PermissionApplication_data = {}
+PermissionApplication_data = {
+    '040818c3-a320-480b-b9bf-60f47dd52e19': {
+        'id': '040818c3-a320-480b-b9bf-60f47dd52e19',
+        'app_id': '50348927-2c4f-4023-b638-445469c66953',
+        'permission': 'create__hr__employee',
+    },
+    '0e6b741e-b886-47dd-92c5-1b3f59a68291': {
+        'id': '0e6b741e-b886-47dd-92c5-1b3f59a68291',
+        'app_id': '50348927-2c4f-4023-b638-445469c66953',
+        'permission': 'edit__hr__employee',
+    },
+    '97b92f5a-0a17-4091-8684-973f830a634a': {
+        'id': '97b92f5a-0a17-4091-8684-973f830a634a',
+        'app_id': '50348927-2c4f-4023-b638-445469c66953',
+        'permission': 'view__hr__employee',
+    },
+    '9cea8790-c8a9-4453-b73f-c11b55f53c68': {
+        'id': '9cea8790-c8a9-4453-b73f-c11b55f53c68',
+        'app_id': '50348927-2c4f-4023-b638-445469c66953',
+        'permission': 'delete__hr__employee',
+    },
+    'f3f3ae17-4510-46d9-9564-f4144fe10331': {
+        'id': 'f3f3ae17-4510-46d9-9564-f4144fe10331',
+        'app_id': '4cdaabcc-09ae-4c13-bb4e-c606eb335b11',
+        'permission': 'create__hr__role',
+    },
+    'b0e3aafd-2c1a-4e79-857b-30da8a9f377d': {
+        'id': 'b0e3aafd-2c1a-4e79-857b-30da8a9f377d',
+        'app_id': '4cdaabcc-09ae-4c13-bb4e-c606eb335b11',
+        'permission': 'edit__hr__role',
+    },
+    'de830f40-c9a6-4d50-b8de-3caddd5e8308': {
+        'id': 'de830f40-c9a6-4d50-b8de-3caddd5e8308',
+        'app_id': '4cdaabcc-09ae-4c13-bb4e-c606eb335b11',
+        'permission': 'view__hr__role',
+    },
+    '0cf5ba77-8ab4-4b91-b516-a95e47c6ef2c': {
+        'id': '0cf5ba77-8ab4-4b91-b516-a95e47c6ef2c',
+        'app_id': '4cdaabcc-09ae-4c13-bb4e-c606eb335b11',
+        'permission': 'delete__hr__role',
+    },
+    '61acb5ee-cf53-4dec-b476-9635ae002075': {
+        'id': '61acb5ee-cf53-4dec-b476-9635ae002075',
+        'app_id': '828b785a-8f57-4a03-9f90-e0edf96560d7',
+        'permission': 'create__saledata__contact',
+    },
+    '87158af6-e829-4550-96d9-dafadeab71d5': {
+        'id': '87158af6-e829-4550-96d9-dafadeab71d5',
+        'app_id': '828b785a-8f57-4a03-9f90-e0edf96560d7',
+        'permission': 'view__saledata__contact',
+    },
+    '0e77a049-d9c3-4253-89d2-7df8fc9438ac': {
+        'id': '0e77a049-d9c3-4253-89d2-7df8fc9438ac',
+        'app_id': '828b785a-8f57-4a03-9f90-e0edf96560d7',
+        'permission': 'edit__saledata__contact',
+    },
+    '29cc98c0-cd5a-4232-b852-5b057206bf6a': {
+        'id': '29cc98c0-cd5a-4232-b852-5b057206bf6a',
+        'app_id': '828b785a-8f57-4a03-9f90-e0edf96560d7',
+        'permission': 'delete__saledata__contact',
+    },
+    '584b6eb9-02fa-4c3d-aae4-3d69e9dc88db': {
+        'id': '584b6eb9-02fa-4c3d-aae4-3d69e9dc88db',
+        'app_id': '4e48c863-861b-475a-aa5e-97a4ed26f294',
+        'permission': 'view__saledata__account',
+    },
+    '4715941a-fc6c-480f-9d8d-e39a189e86b7': {
+        'id': '4715941a-fc6c-480f-9d8d-e39a189e86b7',
+        'app_id': '4e48c863-861b-475a-aa5e-97a4ed26f294',
+        'permission': 'create__saledata__account',
+    },
+    'fb9f971b-7f08-4322-8753-fadd3e3fa2fd': {
+        'id': 'fb9f971b-7f08-4322-8753-fadd3e3fa2fd',
+        'app_id': '4e48c863-861b-475a-aa5e-97a4ed26f294',
+        'permission': 'edit__saledata__account',
+    },
+    '56d8ffcc-5438-4376-8665-f7370417eb1b': {
+        'id': '56d8ffcc-5438-4376-8665-f7370417eb1b',
+        'app_id': '4e48c863-861b-475a-aa5e-97a4ed26f294',
+        'permission': 'delete__saledata__account',
+    },
+}
+
+ApplicationProperty_data = {
+    # Employee
+    'fc7fcfcc-5294-4c2f-bdcb-fa0d8076d211': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'first name',
+        'code': 'first_name',
+        'type': 1,
+    },
+    '9a17bbfa-f555-4996-957c-e2437ecc41ab': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'last name',
+        'code': 'last_name',
+        'type': 1,
+    },
+    'e35a0c33-0482-486a-b45d-0eacde759242': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'email',
+        'code': 'email',
+        'type': 1,
+    },
+    '3358a14e-12c6-417b-bc7e-bce1538cd83f': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'phone',
+        'code': 'phone',
+        'type': 1,
+    },
+    '6b6713e1-3d0b-415e-ba91-4cdcf12ec2db': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'code',
+        'code': 'code',
+        'type': 1,
+    },
+    '757f8311-793c-4e41-a24b-d1651354ae9a': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'user',
+        'code': 'user',
+        'type': 5,
+        'content_type': 'account_User',
+        'properties': {
+            "dropdown_list": {
+                "url": "/account/users/api",
+                "prefix": "user_list",
+                "multiple": "false"
+            }
+        },
+    },
+    'ab9e114a-47ed-4cc4-9a4e-89ffa54afa07': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'group',
+        'code': 'group',
+        'type': 5,
+        'content_type': 'hr_Group',
+        'properties': {
+            "dropdown_list": {
+                "url": "/hr/group/api",
+                "prefix": "group_list",
+                "multiple": "false"
+            }
+        },
+    },
+    'f205394a-8bc2-4d43-8d14-78524d1fae56': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'employee inherit',
+        'code': 'employee_inherit',
+        'type': 5,
+        'content_type': 'hr_Employee',
+        'properties': {
+            "dropdown_list": {
+                "url": "/hr/employee/api",
+                "prefix": "employee_list",
+                "multiple": "false"
+            }
+        },
+    },
+    '1497e99f-89e2-4632-b698-cd4ab3f22d19': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'date joined',
+        'code': 'date_joined',
+        'type': 2,
+    },
+    '3d8ea97e-6b9c-43e3-a584-369cfe4bdfaf': {
+        'application_id': '50348927-2c4f-4023-b638-445469c66953',
+        'title': 'date of birth',
+        'code': 'dob',
+        'type': 2,
+    },
+}
