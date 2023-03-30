0dd959cfe042b3c9577219d0a3a0f7925ac3de5a
haind <nguyenduonghai07@gmail.com>
2023-03-30 23:49:48 +0700

    [feature/ms153] update API

--- apps/core/company/apps.py
+++ apps/core/company/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
 
 
 class CompanyConfig(AppConfig):
@@ -8,3 +7,4 @@ class CompanyConfig(AppConfig):
 
     def ready(self):
         from apps.shared.extends.signals import update_stock  # pylint: disable=import-outside-toplevel / C0415
+        # pylint: disable=W0611
