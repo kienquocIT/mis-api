1c1d73b9c104ebdf2f513e8bc667fa4702b2eeb8
phuclh <phuclh@mtsolution.com.vn>
2023-05-16 15:54:09 +0700

    [feature/ms-272] thêm các filterset_fields cho quotation list api

--- apps/sales/quotation/views.py
+++ apps/sales/quotation/views.py
@@ -13,7 +13,7 @@ class QuotationList(
 ):
     permission_classes = [IsAuthenticated]
     queryset = Quotation.objects
-    filterset_fields = []
+    filterset_fields = ['opportunity', 'sale_person']
     serializer_list = QuotationListSerializer
     serializer_create = QuotationCreateSerializer
     serializer_detail = QuotationListSerializer
@@ -68,7 +68,6 @@ class QuotationDetail(
     )
     @mask_view(login_require=True, auth_require=True, code_perm='')
     def get(self, request, *args, **kwargs):
-        self.serializer_class = QuotationDetailSerializer
         return self.retrieve(request, *args, **kwargs)
 
     @swagger_auto_schema(
@@ -78,5 +77,4 @@ class QuotationDetail(
     )
     @mask_view(login_require=True, auth_require=True, code_perm='')
     def put(self, request, *args, **kwargs):
-        self.serializer_class = QuotationUpdateSerializer
         return self.update(request, *args, **kwargs)
