c2d36a4075b2214cf31cbbfa49a62a8be74e62ea
phuclh <phuclh@mtsolution.com.vn>
2023-06-15 12:10:39 +0700

    [feature/ms-274] add property for quotation

--- apps/sales/quotation/models/indicator.py
+++ apps/sales/quotation/models/indicator.py
@@ -1,35 +1,72 @@
-# from django.db import models
-#
-# from apps.shared import SimpleAbstractModel
-#
-#
-# class Indicator(SimpleAbstractModel):
-#     company = models.OneToOneField(
-#         'company.Company',
-#         on_delete=models.CASCADE,
-#         related_name='indicator_company',
+from django.db import models
+
+from apps.shared import SimpleAbstractModel
+
+
+# class ParamSale(SimpleAbstractModel):
+#     application_code = models.CharField(
+#         max_length=100,
+#         blank=True
 #     )
 #     title = models.CharField(
 #         max_length=100,
 #         blank=True
 #     )
-#     description = models.CharField(
-#         max_length=200,
-#         blank=True
-#     )
-#     order = models.IntegerField(
-#         default=1
+#     code = models.CharField(
+#         max_length=100,
+#         blank=True,
+#         help_text="field name in model or id of element define this field in html"
 #     )
 #
 #     class Meta:
-#         verbose_name = 'Indicator'
-#         verbose_name_plural = 'Indicators'
-#         ordering = ('order',)
+#         verbose_name = 'Param Sale'
+#         verbose_name_plural = 'Param Sales'
 #         default_permissions = ()
 #         permissions = ()
-#
-#
-# # SUPPORT INDICATOR
+
+
+class Indicator(SimpleAbstractModel):
+    company = models.OneToOneField(
+        'company.Company',
+        on_delete=models.CASCADE,
+        related_name='indicator_company',
+    )
+    title = models.CharField(
+        max_length=100,
+        blank=True
+    )
+    description = models.CharField(
+        max_length=200,
+        blank=True
+    )
+    application_code = models.CharField(
+        max_length=100,
+        blank=True
+    )
+    is_indicator_param = models.BooleanField(default=False)
+    is_sale_param = models.BooleanField(default=False)
+    is_math_operator = models.BooleanField(default=False)
+    formula_indicator_params = models.JSONField(
+        default=list,
+        help_text="setup by indicator params, examples: ['indicatorID1', '-', 'indicatorID2', '+', 'indicatorID3',...]"
+    )
+    formula_sale_params = models.JSONField(
+        default=list,
+        help_text="setup by sale params, examples: ['saleID1', '-', 'saleID2', '+', 'saleID3',...]"
+    )
+    order = models.IntegerField(
+        default=1
+    )
+
+    class Meta:
+        verbose_name = 'Indicator'
+        verbose_name_plural = 'Indicators'
+        ordering = ('order',)
+        default_permissions = ()
+        permissions = ()
+
+
+# SUPPORT INDICATOR
 # class IndicatorFormula(SimpleAbstractModel):
 #     indicator = models.ForeignKey(
 #         Indicator,
@@ -37,33 +74,65 @@
 #         verbose_name="indicator",
 #         related_name="indicator_formula_indicator",
 #     )
-#
-#
-#
-# class QuotationIndicator(SimpleAbstractModel):
-#     quotation = models.ForeignKey(
-#         'quotation.Quotation',
-#         on_delete=models.CASCADE,
-#         verbose_name="quotation",
-#         related_name="quotation_indicator_quotation",
-#     )
-#     indicator = models.ForeignKey(
+#     indicator_param = models.ForeignKey(
 #         Indicator,
 #         on_delete=models.CASCADE,
 #         verbose_name="indicator",
-#         related_name="quotation_indicator_indicator",
+#         related_name="indicator_formula_indicator_param",
+#         null=True
 #     )
-#     indicator_result = models.FloatField(
-#         default=0,
-#         help_text="result of specific indicator for quotation"
+#     sale_param = models.ForeignKey(
+#         ParamSale,
+#         on_delete=models.CASCADE,
+#         verbose_name="sale params",
+#         related_name="indicator_formula_quotation_param",
+#         help_text="params of application, defined in model ParamSale, filter by application_code",
+#         null=True
+#     )
+#     math_operator = models.CharField(
+#         max_length=10,
+#         blank=True
 #     )
 #     order = models.IntegerField(
 #         default=1
 #     )
 #
 #     class Meta:
-#         verbose_name = 'Quotation Indicator'
-#         verbose_name_plural = 'Quotation Indicators'
+#         verbose_name = 'Indicator Formula'
+#         verbose_name_plural = 'Indicator Formulas'
 #         ordering = ('order',)
 #         default_permissions = ()
 #         permissions = ()
+
+
+class QuotationIndicator(SimpleAbstractModel):
+    quotation = models.ForeignKey(
+        'quotation.Quotation',
+        on_delete=models.CASCADE,
+        verbose_name="quotation",
+        related_name="quotation_indicator_quotation",
+    )
+    indicator = models.ForeignKey(
+        Indicator,
+        on_delete=models.CASCADE,
+        verbose_name="indicator",
+        related_name="quotation_indicator_indicator",
+    )
+    indicator_result = models.FloatField(
+        default=0,
+        help_text="result of specific indicator for quotation"
+    )
+    indicator_rate_result = models.FloatField(
+        default=0,
+        help_text="rate result of specific indicator for quotation"
+    )
+    order = models.IntegerField(
+        default=1
+    )
+
+    class Meta:
+        verbose_name = 'Quotation Indicator'
+        verbose_name_plural = 'Quotation Indicators'
+        ordering = ('order',)
+        default_permissions = ()
+        permissions = ()
--- apps/sales/quotation/views.py
+++ apps/sales/quotation/views.py
@@ -1,9 +1,10 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 
-from apps.sales.quotation.models import Quotation, QuotationExpense, QuotationAppConfig
+from apps.sales.quotation.models import Quotation, QuotationExpense, QuotationAppConfig, Indicator
 from apps.sales.quotation.serializers.quotation_config import QuotationConfigDetailSerializer, \
     QuotationConfigUpdateSerializer
+from apps.sales.quotation.serializers.quotation_indicator import IndicatorListSerializer
 from apps.sales.quotation.serializers.quotation_serializers import QuotationListSerializer, QuotationCreateSerializer, \
     QuotationDetailSerializer, QuotationUpdateSerializer, QuotationExpenseListSerializer
 from apps.shared import BaseListMixin, mask_view, BaseCreateMixin, BaseRetrieveMixin, BaseUpdateMixin
@@ -127,3 +128,38 @@ class QuotationConfigDetail(BaseRetrieveMixin, BaseUpdateMixin):
         self.lookup_field = 'company_id'
         self.kwargs['company_id'] = request.user.company_current_id
         return self.update(request, *args, **kwargs)
+
+
+# Indicator
+class QuotationIndicatorList(
+    BaseListMixin,
+    BaseCreateMixin
+):
+    permission_classes = [IsAuthenticated]
+    queryset = Indicator.objects
+    filterset_fields = ['application_code']
+    serializer_list = IndicatorListSerializer
+    # serializer_create = QuotationCreateSerializer
+    # serializer_detail = QuotationListSerializer
+    list_hidden_field = ['company_id']
+    # create_hidden_field = ['company_id']
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+    @swagger_auto_schema(
+        operation_summary="Quotation Indicator List",
+        operation_description="Get Quotation Indicator List",
+    )
+    @mask_view(login_require=True, auth_require=True, code_perm='')
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    # @swagger_auto_schema(
+    #     operation_summary="Create Quotation",
+    #     operation_description="Create new Quotation",
+    #     request_body=QuotationCreateSerializer,
+    # )
+    # @mask_view(login_require=True, auth_require=True, code_perm='')
+    # def post(self, request, *args, **kwargs):
+    #     return self.create(request, *args, **kwargs)
