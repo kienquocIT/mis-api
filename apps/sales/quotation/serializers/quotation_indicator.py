24e0e59d818597fb608d4340d4c376f784945d4f
phuclh <phuclh@mtsolution.com.vn>
2023-07-03 14:44:53 +0700

    [feature/ms-274] Thêm API restore indicator company

--- apps/sales/quotation/serializers/quotation_indicator.py
+++ apps/sales/quotation/serializers/quotation_indicator.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from apps.sales.quotation.models import QuotationIndicatorConfig
+from apps.shared.extends.signals import ConfigDefaultData
 
 
 class IndicatorListSerializer(serializers.ModelSerializer):
@@ -71,4 +72,21 @@ class IndicatorUpdateSerializer(serializers.ModelSerializer):
         for key, value in validated_data.items():
             setattr(instance, key, value)
         instance.save()
+        return instance
+
+
+class IndicatorCompanyRestoreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = QuotationIndicatorConfig
+        fields = ('title',)
+
+    @staticmethod
+    def restore_company_indicator(company_obj):
+        QuotationIndicatorConfig.objects.filter(company_id=company_obj.id).delete()
+        ConfigDefaultData(company_obj).quotation_indicator_config()
+        return True
+
+    def update(self, instance, validated_data):
+        self.restore_company_indicator(company_obj=instance.company)
         return instance
