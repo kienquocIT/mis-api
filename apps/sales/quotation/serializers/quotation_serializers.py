c6b6f1301f4afe433cce8df073b7ad71fefceb23
phuclh <phuclh@mtsolution.com.vn>
2023-07-19 11:28:38 +0700

    [feature/ms-274] fix pylint

--- apps/sales/quotation/serializers/quotation_serializers.py
+++ apps/sales/quotation/serializers/quotation_serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.core.workflow.tasks import decorator_run_workflow
+# from apps.core.workflow.tasks import decorator_run_workflow
 from apps.sales.quotation.models import Quotation, QuotationProduct, QuotationTerm, QuotationLogistic, \
     QuotationCost, QuotationExpense, QuotationIndicator
 from apps.sales.quotation.serializers.quotation_sub import QuotationCommonCreate, QuotationCommonValidate
