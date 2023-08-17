2446413e58712df1c309ac00561519241e117855
haind <nguyenduonghai07@gmail.com>
2023-08-17 10:10:43 +0700

    [feature/ms-411] update API

--- apps/sales/quotation/serializers/quotation_serializers.py
+++ apps/sales/quotation/serializers/quotation_serializers.py
@@ -540,13 +540,11 @@ class QuotationListSerializerForCashOutFlow(serializers.ModelSerializer):
 
     @classmethod
     def get_sale_person(cls, obj):
-        if obj.sale_person:
-            return {
-                'id': obj.sale_person_id,
-                'full_name': obj.sale_person.get_full_name(2),
-                'code': obj.sale_person.code,
-            }
-        return {}
+        return {
+            'id': obj.employee_inherit_id,
+            'full_name': obj.employee_inherit.get_full_name(2),
+            'code': obj.employee_inherit.code,
+        } if obj.employee_inherit else {}
 
     @classmethod
     def get_system_status(cls, obj):
