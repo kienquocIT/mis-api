0264dfa4e5afe34e7b79c37e6c37a7068ceb5881
haind <nguyenduonghai07@gmail.com>
2023-09-07 09:52:27 +0700

    [feature/ms-411] update Account testcase

--- apps/masterdata/saledata/tests.py
+++ apps/masterdata/saledata/tests.py
@@ -70,13 +70,12 @@ class AccountTestCase(AdvanceTestCase):
         self.assertCountEqual(
             response.data['result'],
             [
-                'id', 'name', 'website', 'code', 'account_type', 'manager', 'owner', 'phone', 'shipping_address',
+                'id', 'name', 'website', 'code', 'account_type', 'manager', 'phone', 'shipping_address',
                 'billing_address', 'parent_account_mapped', 'account_group', 'tax_code', 'industry', 'total_employees',
                 'email', 'payment_term_customer_mapped', 'payment_term_supplier_mapped',
                 'credit_limit_customer', 'credit_limit_supplier', 'currency', 'contact_mapped',
                 'account_type_selection', 'bank_accounts_mapped', 'credit_cards_mapped',
-                'annual_revenue', 'price_list_mapped',
-                'workflow_runtime_id', 'system_status'
+                'annual_revenue', 'price_list_mapped', 'workflow_runtime_id', 'system_status'
             ],
             check_sum_second=True,
         )
