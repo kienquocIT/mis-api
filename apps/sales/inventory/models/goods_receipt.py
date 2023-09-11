7eef473c17624a4f561bca14d1bbc2227adfcbbe
phuclh <phuclh@mtsolution.com.vn>
2023-09-11 17:09:05 +0700

    [feature/ms-274] create app inventory, chua add migrations

---
+++ apps/sales/inventory/models/goods_receipt.py
@@ -0,0 +1,222 @@
+from django.db import models
+
+from apps.shared import DataAbstractModel, SimpleAbstractModel, GOODS_RECEIPT_TYPE
+
+
+class GoodsReceipt(DataAbstractModel):
+    goods_receipt_type = models.SmallIntegerField(
+        default=0,
+        help_text='choices= ' + str(GOODS_RECEIPT_TYPE),
+    )
+    purchase_order = models.ForeignKey(
+        'purchasing.PurchaseOrder',
+        on_delete=models.CASCADE,
+        verbose_name="purchase order",
+        related_name="goods_receipt_po",
+        null=True,
+    )
+    supplier = models.ForeignKey(
+        'saledata.Account',
+        on_delete=models.CASCADE,
+        verbose_name="supplier",
+        related_name="goods_receipt_supplier",
+        null=True,
+        help_text="sale data Accounts have type supplier"
+    )
+    purchase_requests = models.ManyToManyField(
+        'purchasing.PurchaseRequest',
+        through="GoodsReceiptPurchaseRequest",
+        symmetrical=False,
+        blank=True,
+        related_name='goods_receipt_map_pr'
+    )
+
+    class Meta:
+        verbose_name = 'Goods Receipt'
+        verbose_name_plural = 'Goods Receipts'
+        ordering = ('-date_created',)
+        default_permissions = ()
+        permissions = ()
+
+
+class GoodsReceiptPurchaseRequest(SimpleAbstractModel):
+    goods_receipt = models.ForeignKey(
+        GoodsReceipt,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt",
+        related_name="goods_receipt_pr_goods_receipt",
+    )
+    purchase_request = models.ForeignKey(
+        'purchasing.PurchaseRequest',
+        on_delete=models.CASCADE,
+        verbose_name="purchase request",
+        related_name="goods_receipt_pr_pr",
+    )
+
+    class Meta:
+        verbose_name = 'Goods Receipt Purchase Request'
+        verbose_name_plural = 'Goods Receipt Purchase Requests'
+        ordering = ()
+        default_permissions = ()
+        permissions = ()
+
+
+class GoodsReceiptProduct(SimpleAbstractModel):
+    goods_receipt = models.ForeignKey(
+        GoodsReceipt,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt",
+        related_name="goods_receipt_product_goods_receipt",
+    )
+    purchase_order_product = models.ForeignKey(
+        'purchasing.PurchaseOrderProduct',
+        on_delete=models.CASCADE,
+        verbose_name="po product",
+        related_name="goods_receipt_product_po_product",
+        null=True
+    )
+    product = models.ForeignKey(
+        'saledata.Product',
+        on_delete=models.CASCADE,
+        verbose_name="product",
+        related_name="goods_receipt_product_product",
+        null=True
+    )
+    uom = models.ForeignKey(
+        'saledata.UnitOfMeasure',
+        on_delete=models.CASCADE,
+        verbose_name="unit of measure",
+        related_name="goods_receipt_product_uom",
+        null=True
+    )
+    tax = models.ForeignKey(
+        'saledata.Tax',
+        on_delete=models.CASCADE,
+        verbose_name="tax",
+        related_name="goods_receipt_product_tax",
+        null=True
+    )
+    quantity_import = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name = 'Goods Receipt Product'
+        verbose_name_plural = 'Goods Receipt Products'
+        ordering = ()
+        default_permissions = ()
+        permissions = ()
+
+
+class GoodsReceiptRequestProduct(SimpleAbstractModel):
+    goods_receipt = models.ForeignKey(
+        GoodsReceipt,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt",
+        related_name="goods_receipt_request_product_goods_receipt",
+    )
+    goods_receipt_product = models.ForeignKey(
+        GoodsReceiptProduct,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt product",
+        related_name="goods_receipt_request_product_gr_product",
+        null=True
+    )
+    purchase_request_product = models.ForeignKey(
+        'purchasing.PurchaseRequestProduct',
+        on_delete=models.CASCADE,
+        verbose_name="purchase request product",
+        related_name="goods_receipt_request_product_pr_product",
+        null=True
+    )
+    quantity_import = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name = 'Goods Receipt Request Product'
+        verbose_name_plural = 'Goods Receipt Request Products'
+        ordering = ()
+        default_permissions = ()
+        permissions = ()
+
+
+class GoodsReceiptWarehouse(SimpleAbstractModel):
+    goods_receipt = models.ForeignKey(
+        GoodsReceipt,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt",
+        related_name="goods_receipt_warehouse_goods_receipt",
+    )
+    goods_receipt_request_product = models.ForeignKey(
+        GoodsReceiptRequestProduct,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt request product",
+        related_name="goods_receipt_warehouse_request_product",
+        null=True
+    )
+    warehouse = models.ForeignKey(
+        'saledata.WareHouse',
+        on_delete=models.CASCADE,
+        verbose_name="warehouse",
+        related_name="goods_receipt_warehouse_warehouse",
+        null=True
+    )
+    quantity_import = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name = 'Goods Receipt Warehouse'
+        verbose_name_plural = 'Goods Receipt Warehouses'
+        ordering = ()
+        default_permissions = ()
+        permissions = ()
+
+
+class GoodsReceiptLot(SimpleAbstractModel):
+    goods_receipt = models.ForeignKey(
+        GoodsReceipt,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt",
+        related_name="goods_receipt_lot_goods_receipt",
+    )
+    goods_receipt_warehouse = models.ForeignKey(
+        GoodsReceiptWarehouse,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt warehouse",
+        related_name="goods_receipt_lot_gr_warehouse",
+    )
+    lot_number = models.CharField(max_length=100, blank=True, null=True)
+    quantity_import = models.FloatField(default=0)
+    expire_date = models.DateTimeField(null=True)
+    manufacture_date = models.DateTimeField(null=True)
+
+    class Meta:
+        verbose_name = 'Goods Receipt Lot'
+        verbose_name_plural = 'Goods Receipt Lots'
+        ordering = ()
+        default_permissions = ()
+        permissions = ()
+
+
+class GoodsReceiptSerial(SimpleAbstractModel):
+    goods_receipt = models.ForeignKey(
+        GoodsReceipt,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt",
+        related_name="goods_receipt_serial_goods_receipt",
+    )
+    goods_receipt_warehouse = models.ForeignKey(
+        GoodsReceiptWarehouse,
+        on_delete=models.CASCADE,
+        verbose_name="goods receipt warehouse",
+        related_name="goods_receipt_serial_gr_warehouse",
+    )
+    vendor_serial_number = models.CharField(max_length=100, blank=True, null=True)
+    serial_number = models.CharField(max_length=100, blank=True, null=True)
+    expire_date = models.DateTimeField(null=True)
+    manufacture_date = models.DateTimeField(null=True)
+    warranty_start = models.DateTimeField(null=True)
+    warranty_end = models.DateTimeField(null=True)
+
+    class Meta:
+        verbose_name = 'Goods Receipt Serial'
+        verbose_name_plural = 'Goods Receipt Serials'
+        ordering = ()
+        default_permissions = ()
+        permissions = ()
---
+++ apps/sales/inventory/serializers/goods_receipt.py
@@ -0,0 +1,103 @@
+from rest_framework import serializers
+
+from apps.core.workflow.tasks import decorator_run_workflow
+from apps.sales.inventory.models import GoodsReceipt
+from apps.shared import SYSTEM_STATUS
+
+
+# GOODS RECEIPT BEGIN
+class GoodsReceiptListSerializer(serializers.ModelSerializer):
+    supplier = serializers.SerializerMethodField()
+    system_status = serializers.SerializerMethodField()
+
+    class Meta:
+        model = GoodsReceipt
+        fields = (
+            'id',
+            'title',
+            'code',
+            'supplier',
+            'system_status',
+        )
+
+    @classmethod
+    def get_supplier(cls, obj):
+        return {
+            'id': obj.supplier_id,
+            'name': obj.supplier.name,
+            'code': obj.supplier.code,
+        } if obj.supplier else {}
+
+    @classmethod
+    def get_system_status(cls, obj):
+        if obj.system_status or obj.system_status == 0:
+            return dict(SYSTEM_STATUS).get(obj.system_status)
+        return None
+
+
+class GoodsReceiptDetailSerializer(serializers.ModelSerializer):
+    supplier = serializers.SerializerMethodField()
+
+    class Meta:
+        model = GoodsReceipt
+        fields = (
+            'id',
+            'title',
+            'code',
+            'supplier',
+            # system
+            'system_status',
+            'workflow_runtime_id',
+        )
+
+    @classmethod
+    def get_supplier(cls, obj):
+        return {
+            'id': obj.supplier_id,
+            'name': obj.supplier.name,
+            'code': obj.supplier.code,
+        } if obj.supplier else {}
+
+    @classmethod
+    def get_system_status(cls, obj):
+        if obj.system_status or obj.system_status == 0:
+            return dict(SYSTEM_STATUS).get(obj.system_status)
+        return None
+
+
+class GoodsReceiptCreateSerializer(serializers.ModelSerializer):
+    title = serializers.CharField()
+    supplier = serializers.UUIDField(required=False)
+
+    class Meta:
+        model = GoodsReceipt
+        fields = (
+            'title',
+            'supplier',
+            # system
+            'system_status',
+        )
+
+    @decorator_run_workflow
+    def create(self, validated_data):
+        goods_receipt = GoodsReceipt.objects.create(**validated_data)
+        return goods_receipt
+
+
+class GoodsReceiptUpdateSerializer(serializers.ModelSerializer):
+    supplier = serializers.UUIDField(required=False)
+
+    class Meta:
+        model = GoodsReceipt
+        fields = (
+            'title',
+            'supplier',
+            # system
+            'system_status',
+        )
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
