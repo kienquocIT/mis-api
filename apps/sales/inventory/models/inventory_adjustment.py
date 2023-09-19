14544c89e59128ba051991c39fd21e8cbbee1cb9
haind <nguyenduonghai07@gmail.com>
2023-09-19 11:11:18 +0700

    [feature/ms-412] inventory adjustment API

---
+++ apps/sales/inventory/models/inventory_adjustment.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+from apps.masterdata.saledata.models import ProductWareHouse
+from apps.shared import DataAbstractModel, SimpleAbstractModel, GOODS_RECEIPT_TYPE
+
+
+class InventoryAdjustment(DataAbstractModel):
+    warehouses_mapped = models.ManyToManyField(
+        'saledata.WareHouse',
+        through='InventoryAdjustmentWarehouse',
+        symmetrical=False,
+        blank=True,
+        related_name='warehouses_mapped_ia'
+    )
+    employees_in_charge_mapped = models.ManyToManyField(
+        'hr.Employee',
+        through="InventoryAdjustmentEmployeeInCharge",
+        symmetrical=False,
+        blank=True,
+        related_name='employees_in_charge_mapped_ia'
+    )
+
+    class Meta:
+        verbose_name = 'Inventory Adjustment'
+        verbose_name_plural = 'Inventory Adjustments'
+        ordering = ('-date_created',)
+        default_permissions = ()
+        permissions = ()
+
+
+class InventoryAdjustmentWarehouse(SimpleAbstractModel):
+    inventory_adjustment_mapped = models.ForeignKey(InventoryAdjustment, on_delete=models.CASCADE)
+    warehouse_mapped = models.ForeignKey('saledata.WareHouse', on_delete=models.CASCADE)
+
+
+class InventoryAdjustmentEmployeeInCharge(SimpleAbstractModel):
+    inventory_adjustment_mapped = models.ForeignKey(InventoryAdjustment, on_delete=models.CASCADE)
+    employee_mapped = models.ForeignKey('hr.Employee', on_delete=models.CASCADE)
+
+
+class InventoryAdjustmentItem(SimpleAbstractModel):
+    inventory_adjustment_mapped = models.ForeignKey(
+        InventoryAdjustment,
+        on_delete=models.CASCADE,
+        related_name='inventory_adjustment_item_mapped'
+    )
+    product_mapped = models.ForeignKey('saledata.Product', on_delete=models.CASCADE)
+    warehouse_mapped = models.ForeignKey('saledata.WareHouse', on_delete=models.CASCADE)
+    uom_mapped = models.ForeignKey('saledata.UnitOfMeasure', on_delete=models.CASCADE)
+    book_quantity = models.IntegerField()
+    count = models.IntegerField()
+    select_for_action = models.BooleanField(default=False)
+    action_status = models.BooleanField(default=False)
--- apps/sales/inventory/urls.py
+++ apps/sales/inventory/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from apps.sales.inventory.views.goods_receipt import GoodsReceiptList, GoodsReceiptDetail
+from apps.sales.inventory.views.inventory_adjustment import InventoryAdjustmentList, InventoryAdjustmentDetail
 
 urlpatterns = [
     # good receipt
     path('goods-receipt/list', GoodsReceiptList.as_view(), name='GoodsReceiptList'),
     path('goods-receipt/<str:pk>', GoodsReceiptDetail.as_view(), name='GoodsReceiptDetail'),
+    # inventory adjustment
+    path('inventory-adjustments', InventoryAdjustmentList.as_view(), name='InventoryAdjustmentList'),
+    path('inventory-adjustment/<str:pk>', InventoryAdjustmentDetail.as_view(), name='InventoryAdjustmentDetail'),
 ]
