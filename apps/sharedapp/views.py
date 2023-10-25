048ba5425743b837911c0953da76f4c094713b1a
trieunt <trieunt@mtsolution.com.vn>
2023-10-25 18:35:34 +0700

    [SIT] add plan_by_id for system data app.

--- apps/sharedapp/views.py
+++ apps/sharedapp/views.py
@@ -31,26 +31,42 @@ class PlanApplicationConfigData(APIView):
         if plan_id and TypeCheck.check_uuid(plan_id):
             filter_kw['plan_id'] = plan_id
 
-        result = {}
+        plan_app = {}
         for obj in PlanApplication.objects.select_related('plan', 'application').filter(**filter_kw):
             plan_id = str(obj.plan_id)
-            if plan_id not in result:
-                result[plan_id] = {
+            if plan_id not in plan_app:
+                plan_app[plan_id] = {
                     'id': plan_id,
                     'title': obj.plan.title,
                     'code': obj.plan.code,
                     'application': [],
                 }
-            result[plan_id]['application'].append({
-                'id': obj.application_id,
-                'title': obj.application.title,
-                'code': obj.application.code,
-                'app_label': obj.application.app_label,
-                'model_code': obj.application.model_code,
-                'is_workflow': obj.application.is_workflow,
-                'permit_mapping': obj.application.permit_mapping,
-                'depend_follow_main': obj.application.depend_follow_main,
-                'filtering_inheritor': obj.application.filtering_inheritor,
-            })
+            plan_app[plan_id]['application'].append(
+                {
+                    'id': obj.application_id,
+                    'title': obj.application.title,
+                    'code': obj.application.code,
+                    'app_label': obj.application.app_label,
+                    'model_code': obj.application.model_code,
+                    'is_workflow': obj.application.is_workflow,
+                    'permit_mapping': obj.application.permit_mapping,
+                    'depend_follow_main': obj.application.depend_follow_main,
+                    'filtering_inheritor': obj.application.filtering_inheritor,
+                }
+            )
 
-        return ResponseController.success_200(data=[value for _key, value in result.items()], key_data='result')
+        app_by_id = {
+            str(obj.id): {
+                "id": str(obj.id),
+                "title": str(obj.title),
+                "code": str(obj.code),
+            }
+            for obj in Application.objects.filter()
+        }
+
+        return ResponseController.success_200(
+            data={
+                'plan_app': [value for _key, value in plan_app.items()],
+                'app_by_id': app_by_id,
+            }, key_data='result'
+        )
