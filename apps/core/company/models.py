4f78578e840687c441e1820b373d6afb5241cd03
haind <nguyenduonghai07@gmail.com>
2023-10-31 10:13:55 +0700

    [feature/ms-416] pylint

--- apps/core/company/models.py
+++ apps/core/company/models.py
@@ -4,9 +4,11 @@ from uuid import UUID
 from jsonfield import JSONField
 from django.db import models
 from django.conf import settings
-from apps.shared import SimpleAbstractModel, MasterDataAbstractModel, CURRENCY_MASK_MONEY, MediaForceAPI
-from apps.core.models import CoreAbstractModel
 from django.utils.translation import gettext_lazy as _
+from apps.shared import (
+    SimpleAbstractModel, MasterDataAbstractModel, CURRENCY_MASK_MONEY, MediaForceAPI
+)
+from apps.core.models import CoreAbstractModel
 
 
 DEFINITION_INVENTORY_VALUATION_CHOICES = [
