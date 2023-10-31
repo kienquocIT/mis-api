aa2ed86264b1916e1e429a8c965693e6a28804d8
phuclh <phuclh@mtsolution.com.vn>
2023-10-31 09:21:33 +0700

    [feature/ms-274] fix pylint

--- apps/shared/extends/signals.py
+++ apps/shared/extends/signals.py
@@ -24,8 +24,7 @@ from apps.sales.opportunity.models import (
 )
 from apps.sales.purchasing.models import PurchaseRequestConfig
 from apps.sales.quotation.models import (
-    QuotationAppConfig, ConfigShortSale, ConfigLongSale, QuotationIndicatorConfig,
-    IndicatorDefaultData,
+    QuotationAppConfig, ConfigShortSale, ConfigLongSale, QuotationIndicatorConfig, SQIndicatorDefaultData,
 )
 from apps.core.base.models import Currency as BaseCurrency
 from apps.core.company.models import Company, CompanyConfig
@@ -35,7 +34,7 @@ from apps.masterdata.saledata.models import (
 from apps.sales.delivery.models import DeliveryConfig
 from apps.sales.saleorder.models import (
     SaleOrderAppConfig, ConfigOrderLongSale, ConfigOrderShortSale,
-    SaleOrderIndicatorConfig,
+    SaleOrderIndicatorConfig, ORIndicatorDefaultData,
 )
 from apps.sales.task.models import OpportunityTaskConfig, OpportunityTaskStatus
 
@@ -597,7 +596,7 @@ class ConfigDefaultData:
 
     def quotation_indicator_config(self):
         bulk_info = []
-        for data in IndicatorDefaultData.INDICATOR_DATA:
+        for data in SQIndicatorDefaultData.INDICATOR_DATA:
             bulk_info.append(
                 QuotationIndicatorConfig(
                     tenant=self.company_obj.tenant,
@@ -609,7 +608,7 @@ class ConfigDefaultData:
 
     def sale_order_indicator_config(self):
         bulk_info = []
-        for data in IndicatorDefaultData.ORDER_INDICATOR_DATA:
+        for data in ORIndicatorDefaultData.INDICATOR_DATA:
             bulk_info.append(
                 SaleOrderIndicatorConfig(
                     tenant=self.company_obj.tenant,
