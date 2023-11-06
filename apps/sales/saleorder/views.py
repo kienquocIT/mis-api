840b7d95df23ac4466043cfc27d1a3667a87a135
haind <nguyenduonghai07@gmail.com>
2023-11-06 16:49:04 +0700

    [feature/ms-417] Sale Order View update

--- apps/sales/saleorder/views.py
+++ apps/sales/saleorder/views.py
@@ -34,13 +34,6 @@ class SaleOrderList(BaseListMixin, BaseCreateMixin):
     ]
 
     def get_queryset(self):
-        if self.request.query_params.get('filter_quotation', None):
-            return super().get_queryset().select_related(
-                "customer",
-                "opportunity",
-                "quotation",
-                "employee_inherit",
-            ).filter(quotation_id=self.request.query_params['filter_quotation'])
         return super().get_queryset().select_related(
             "customer",
             "opportunity",
