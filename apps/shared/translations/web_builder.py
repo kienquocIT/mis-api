7556c9b39ac8a988f8591635db0f9a319f3b4b75
trieunt <trieunt@mtsolution.com.vn>
2023-11-14 11:38:44 +0700

    [SIT] Fix page builder.

---
+++ apps/shared/translations/web_builder.py
@@ -0,0 +1,8 @@
+from django.utils.translation import gettext_lazy as _
+
+
+class WebBuilderMsg:
+    PATH_START_WITH_FORWARD_SLASH = _('Path must start with "/"')
+    PATH_NOT_ALLOWED_USE = _("Can't use this path")
+    PATH_EXIST = _('Path is exist')
+    FILTER_COMPANY_NOT_EXIST = _('Filtering is incorrect')
