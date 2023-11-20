4421e6d5aada06d54d9340ef8e5e504e9603e62e
phuclh <phuclh@mtsolution.com.vn>
2023-11-20 14:36:27 +0700

    [feature/ms-274] them position choice WF type InWF

--- apps/core/workflow/utils/runtime.py
+++ apps/core/workflow/utils/runtime.py
@@ -399,18 +399,20 @@ class RuntimeStageHandler:
         collab_in_wf = {}
         for collab in CollabInWorkflow.objects.filter(node=node):
             zone_and_properties = cls.__get_zone_and_properties(collab.zone.all())
-            if collab.in_wf_option == 1 and doc_employee_inherit:
+            if collab.in_wf_option == 1 and doc_employee_inherit:  # BY POSITION
                 if not doc_employee_inherit.group:
                     raise ValueError('Employee inherit does not have group')
-                if collab.position_choice == 1:
+                if collab.position_choice == 1:  # 1st manager
                     if not doc_employee_inherit.group.first_manager_id:
                         raise ValueError('1st manager is not defined')
                     collab_in_wf[str(doc_employee_inherit.group.first_manager_id)] = zone_and_properties
-                elif collab.position_choice == 2:
+                elif collab.position_choice == 2:  # 2nd manager
                     if not doc_employee_inherit.group.second_manager_id:
                         raise ValueError('2nd manager is not defined')
                     collab_in_wf[str(doc_employee_inherit.group.second_manager_id)] = zone_and_properties
-            elif collab.in_wf_option == 2:
+                elif collab.position_choice == 3:  # Beneficiary (Document inherit)
+                    collab_in_wf[str(doc_employee_inherit.id)] = zone_and_properties
+            elif collab.in_wf_option == 2:  # BY EMPLOYEE
                 collab_in_wf[str(collab.employee_id)] = zone_and_properties
 
         return collab_in_wf
