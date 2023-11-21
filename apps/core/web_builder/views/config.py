18e2a0a608f203e1bb6cb02a20af225d91a36e3a
trieunt <trieunt@mtsolution.com.vn>
2023-11-21 11:58:51 +0700

    [SIT] update preview page.

--- apps/core/web_builder/views/config.py
+++ apps/core/web_builder/views/config.py
@@ -1,5 +1,8 @@
 import re
+from copy import deepcopy
+from uuid import uuid4
 
+from django.contrib.auth.models import AnonymousUser
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 
@@ -11,7 +14,7 @@ from apps.core.web_builder.serializers.config import (
 )
 from apps.shared import (
     BaseListMixin, BaseCreateMixin, BaseRetrieveMixin, BaseUpdateMixin, BaseDestroyMixin, mask_view,
-    TypeCheck, ResponseController,
+    TypeCheck, ResponseController, StringHandler,
 )
 from apps.core.web_builder.models import PageBuilder
 
@@ -70,7 +73,45 @@ class PageBuilderDetail(BaseRetrieveMixin, BaseUpdateMixin, BaseDestroyMixin):
         return self.destroy(request, *args, pk, **kwargs)
 
 
+class PageBuilderDetailClone(BaseRetrieveMixin):
+    queryset = PageBuilder.objects
+    serializer_detail = PageBuilderDetailSerializer
+
+    @swagger_auto_schema()
+    @mask_view(login_require=True, auth_require=False, allow_admin_company=True)
+    def post(self, request, *args, **kwargs):
+        instance = self.get_object()
+        state_check = self.manual_check_obj_retrieve(instance=instance)  # pylint: disable=E1128
+        if state_check is None:
+            state_check = self.check_perm_by_obj_or_body_data(obj=instance, hidden_field=self.retrieve_hidden_field)
+        if state_check is True:
+            instance_new = deepcopy(instance)
+            instance_new.id = uuid4()
+            instance_new.is_publish = False
+            instance_new.page_path = instance.page_path + ("clone" if instance.page_path.endswith("/") else "/clone")
+            if PageBuilder.objects.filter_current(
+                    fill__tenant=True, fill__company=True, page_path=instance_new.page_path
+            ).exists():
+                instance_new.page_path += StringHandler.random_str(5)
+            instance_new.save()
+
+            return ResponseController.success_200(
+                data=PageBuilderDetailSerializer(instance=instance_new).data, key_data='result'
+            )
+        return ResponseController.forbidden_403()
+
+
 class PageBuilderViewPathSub(APIView):
+    def filter_page_customize(self):
+        page_filter = {
+            'is_publish': True
+        }
+        if self.request.user and hasattr(self.request.user, 'id') and not isinstance(self.request.user, AnonymousUser):
+            view_draft = self.request.query_params.dict().get('view_draft', '0') == '1'
+            if view_draft is True:
+                del page_filter['is_publish']
+        return page_filter
+
     @swagger_auto_schema()
     @mask_view(auth_require=False, login_require=False)
     def get(self, request, *args, pk_company, path_sub, **kwargs):
@@ -81,7 +122,7 @@ class PageBuilderViewPathSub(APIView):
             if not path_sub.startswith('/'):
                 path_sub = '/' + path_sub
 
-            for obj in PageBuilder.objects.filter(company_id=pk_company, is_publish=True).cache():
+            for obj in PageBuilder.objects.filter(company_id=pk_company, **self.filter_page_customize()).cache():
                 regex_str = obj.page_path
                 if '{id}' in obj.page_path:
                     regex_str = str(obj.page_path).replace(
@@ -105,16 +146,18 @@ class CompanyGetter(APIView):
             except Company.DoesNotExist:
                 return ResponseController.notfound_404()
 
-            return ResponseController.success_200(data={
-                'id':  obj.id,
-                'title': obj.title,
-                'code': obj.code,
-                'sub_domain': obj.sub_domain,
-                'tenant': {
-                    'id': obj.tenant.id,
-                    'title': obj.tenant.title,
-                    'code': obj.tenant.code,
-                }
-            }, key_data='result')
+            return ResponseController.success_200(
+                data={
+                    'id': obj.id,
+                    'title': obj.title,
+                    'code': obj.code,
+                    'sub_domain': obj.sub_domain,
+                    'tenant': {
+                        'id': obj.tenant.id,
+                        'title': obj.tenant.title,
+                        'code': obj.tenant.code,
+                    }
+                }, key_data='result'
+            )
 
         return ResponseController.notfound_404()
