2ab3d213d92af4049b4ed9587751bdec30fe7bb3
phuclh <phuclh@mtsolution.com.vn>
2023-11-22 07:59:01 +0700

    [feature/ms-274] update final acceptance

--- apps/sales/acceptance/views.py
+++ apps/sales/acceptance/views.py
@@ -1,8 +1,9 @@
 from drf_yasg.utils import swagger_auto_schema
 
 from apps.sales.acceptance.models import FinalAcceptance
-from apps.sales.acceptance.serializers.final_acceptance import FinalAcceptanceListSerializer
-from apps.shared import mask_view, BaseListMixin
+from apps.sales.acceptance.serializers.final_acceptance import FinalAcceptanceListSerializer, \
+    FinalAcceptanceUpdateSerializer
+from apps.shared import mask_view, BaseListMixin, BaseUpdateMixin, BaseRetrieveMixin
 
 
 # FINAL ACCEPTANCE
@@ -30,3 +31,41 @@ class FinalAcceptanceList(BaseListMixin):
     )
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+
+class FinalAcceptanceDetail(
+    BaseRetrieveMixin,
+    BaseUpdateMixin,
+):
+    queryset = FinalAcceptance.objects
+    serializer_detail = FinalAcceptanceListSerializer
+    serializer_update = FinalAcceptanceUpdateSerializer
+    retrieve_hidden_field = BaseRetrieveMixin.RETRIEVE_HIDDEN_FIELD_DEFAULT
+    update_hidden_field = BaseUpdateMixin.UPDATE_HIDDEN_FIELD_DEFAULT
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+    @swagger_auto_schema(
+        operation_summary="Final Acceptance detail",
+        operation_description="Get Final Acceptance detail by ID",
+    )
+    @mask_view(
+        login_require=True, auth_require=False,
+        label_code='acceptance', model_code='finalacceptance', perm_code='view',
+        opp_enabled=True, prj_enabled=True,
+    )
+    def get(self, request, *args, pk, **kwargs):
+        return self.retrieve(request, *args, pk, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Update Final Acceptance",
+        operation_description="Update Final Acceptance by ID",
+        request_body=FinalAcceptanceUpdateSerializer,
+    )
+    @mask_view(
+        login_require=True, auth_require=False,
+        label_code='acceptance', model_code='finalacceptance', perm_code='edit',
+    )
+    def put(self, request, *args, pk, **kwargs):
+        return self.update(request, *args, pk, **kwargs)
