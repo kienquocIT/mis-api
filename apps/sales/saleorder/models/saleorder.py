dad5b2f5d00e693a40882839aa2e5dd88fd89244
phuclh <phuclh@mtsolution.com.vn>
2023-11-30 10:05:52 +0700

    [feature/ms-274] update function create_final_acceptance

--- apps/sales/saleorder/models/saleorder.py
+++ apps/sales/saleorder/models/saleorder.py
@@ -339,30 +339,21 @@ class SaleOrder(DataAbstractModel):
 
     @classmethod
     def create_final_acceptance(cls, instance):
-        list_data_indicator = []
-        for so_ind in instance.sale_order_indicator_sale_order.all():
-            list_data_indicator.extend([
-                {
-                    'tenant_id': instance.tenant_id,
-                    'company_id': instance.company_id,
-                    'sale_order_indicator_id': so_ind.id,
-                    'indicator_id': so_ind.quotation_indicator_id,
-                    'indicator_value': so_ind.indicator_value,
-                    'different_value': 0 - so_ind.indicator_value,
-                    'rate_value': so_ind.indicator_rate,
-                    'order': so_ind.order,
-                    'is_indicator': True,
-                },
-                {
-                    'tenant_id': instance.tenant_id,
-                    'company_id': instance.company_id,
-                    'sale_order_id': instance.id,
-                    'indicator_id': so_ind.quotation_indicator_id,
-                    'indicator_value': so_ind.indicator_value,
-                    'actual_value': so_ind.indicator_value,
-                    'is_plan': True,
-                }
-            ])
+        list_data_indicator = [
+            {
+                'tenant_id': instance.tenant_id,
+                'company_id': instance.company_id,
+                'sale_order_id': instance.id,
+                'sale_order_indicator_id': so_ind.id,
+                'indicator_id': so_ind.quotation_indicator_id,
+                'indicator_value': so_ind.indicator_value,
+                'different_value': 0 - so_ind.indicator_value,
+                # 'rate_value': so_ind.indicator_rate,
+                'order': so_ind.order,
+                'is_indicator': True,
+            }
+            for so_ind in instance.sale_order_indicator_sale_order.all()
+        ]
         FinalAcceptance.create_final_acceptance_from_so(
             tenant_id=instance.tenant_id,
             company_id=instance.company_id,
