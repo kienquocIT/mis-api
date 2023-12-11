aa668b59e8e520a67b452cf034a9192d4466b16f
phuclh <phuclh@mtsolution.com.vn>
2023-12-11 17:00:48 +0700

    [feature/ms-274] fix pylint

--- apps/core/workflow/utils/runtime_sub.py
+++ apps/core/workflow/utils/runtime_sub.py
@@ -11,13 +11,13 @@ class WFSupportFunctionsHandler:
                 if doc_employee_inherit.group.first_manager_id == doc_employee_inherit.id:
                     return cls.get_manager_upper_group(doc_employee_inherit.group)
                 return doc_employee_inherit.group.first_manager_id
-            elif collab.position_choice == 2:  # 2nd manager
+            if collab.position_choice == 2:  # 2nd manager
                 if not doc_employee_inherit.group.second_manager_id:
                     raise ValueError('2nd manager is not defined')
                 return doc_employee_inherit.group.second_manager_id
-            elif collab.position_choice == 3:  # Beneficiary (Document inherit)
+            if collab.position_choice == 3:  # Beneficiary (Document inherit)
                 return doc_employee_inherit.id
-        elif collab.in_wf_option == 2:  # BY EMPLOYEE
+        if collab.in_wf_option == 2:  # BY EMPLOYEE
             return collab.employee_id
         raise ValueError('Can not find assignee for this node')
 
