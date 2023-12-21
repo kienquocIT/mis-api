6fbda78404f898b4adb9fe1a14e1ef2c3e5b8c0a
haind <haind@mtsolution.com.vn>
2023-12-21 17:43:14 +0700

    [feature/ms-419] pylint

--- apps/sales/cashoutflow/models/payment.py
+++ apps/sales/cashoutflow/models/payment.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from apps.core.company.models import CompanyFunctionNumber
 from apps.sales.acceptance.models import FinalAcceptance
 from apps.sales.cashoutflow.models import AdvancePaymentCost
-from apps.shared import DataAbstractModel, SimpleAbstractModel, AdvancePaymentMsg
+from apps.shared import DataAbstractModel, SimpleAbstractModel
 
 __all__ = [
     'Payment',
