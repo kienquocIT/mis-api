c9a82714e9c145b63cdc899cdb65789fb05d7f06
haind <haind@mtsolution.com.vn>
2024-01-17 11:38:25 +0700

    [feature/ms-424] update API

--- apps/sales/cashoutflow/models/return_advance.py
+++ apps/sales/cashoutflow/models/return_advance.py
@@ -40,10 +40,12 @@ class ReturnAdvance(DataAbstractModel):
     def update_advance_payment_cost(cls, instance):
         # instance.money_received = True
         # instance.save(update_fields=['money_received'])
-        for item in instance.return_advance.all():
-            item.advance_payment_cost.sum_return_value += item.return_value
-            item.advance_payment_cost.save(update_fields=['sum_return_value'])
-        return True
+        if instance.money_received:
+            for item in instance.return_advance.all():
+                item.advance_payment_cost.sum_return_value += item.return_value
+                item.advance_payment_cost.save(update_fields=['sum_return_value'])
+            return True
+        return False
 
     def save(self, *args, **kwargs):
         if self.system_status in [2, 3]:
