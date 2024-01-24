ae77145e086b9cc261f01934dc36fba8e7aa4170
phuclh <phuclh@mtsolution.com.vn>
2024-01-24 17:26:47 +0700

    [feature/ms-274] change field customer ReportCustomer

--- apps/sales/report/models/report_sales.py
+++ apps/sales/report/models/report_sales.py
@@ -115,7 +115,7 @@ class ReportProduct(DataAbstractModel):
 
 
 class ReportCustomer(DataAbstractModel):
-    customer = models.OneToOneField(
+    customer = models.ForeignKey(
         'saledata.Account',
         on_delete=models.CASCADE,
         related_name='report_customer_customer',
@@ -144,24 +144,37 @@ class ReportCustomer(DataAbstractModel):
             gross_profit: float,
             net_income: float,
     ):
-        obj, _created = cls.objects.get_or_create(
-            tenant_id=tenant_id, company_id=company_id, customer_id=customer_id,
-            defaults={
-                'employee_created_id': employee_created_id,
-                'employee_inherit_id': employee_inherit_id,
-                'group_inherit_id': group_inherit_id,
-                'date_approved': date_approved,
-                'revenue': revenue,
-                'gross_profit': gross_profit,
-                'net_income': net_income,
-            }
+        # obj, _created = cls.objects.get_or_create(
+        #     tenant_id=tenant_id, company_id=company_id, customer_id=customer_id,
+        #     defaults={
+        #         'employee_created_id': employee_created_id,
+        #         'employee_inherit_id': employee_inherit_id,
+        #         'group_inherit_id': group_inherit_id,
+        #         'date_approved': date_approved,
+        #         'revenue': revenue,
+        #         'gross_profit': gross_profit,
+        #         'net_income': net_income,
+        #     }
+        # )
+        # if _created is True:
+        #     return True
+        # obj.revenue += revenue
+        # obj.gross_profit += gross_profit
+        # obj.net_income += net_income
+        # obj.save(update_fields=['revenue', 'gross_profit', 'net_income'])
+
+        cls.objects.create(
+            tenant_id=tenant_id,
+            company_id=company_id,
+            customer_id=customer_id,
+            employee_created_id=employee_created_id,
+            employee_inherit_id=employee_inherit_id,
+            group_inherit_id=group_inherit_id,
+            date_approved=date_approved,
+            revenue=revenue,
+            gross_profit=gross_profit,
+            net_income=net_income,
         )
-        if _created is True:
-            return True
-        obj.revenue += revenue
-        obj.gross_profit += gross_profit
-        obj.net_income += net_income
-        obj.save(update_fields=['revenue', 'gross_profit', 'net_income'])
         return True
 
     class Meta:
