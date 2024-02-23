e1e53ac9197d940dfad4fa53a1ce581725e0d1a0
phuclh <phuclh@mtsolution.com.vn>
2024-02-23 10:38:15 +0700

    [feature/ms-274] them sale_order_id vao list_data_indicator function create_final_acceptance()

--- apps/sales/delivery/serializers/delivery.py
+++ apps/sales/delivery/serializers/delivery.py
@@ -549,6 +549,7 @@ class OrderDeliverySubUpdateSerializer(serializers.ModelSerializer):
                 list_data_indicator.append({
                     'tenant_id': instance.tenant_id,
                     'company_id': instance.company_id,
+                    'sale_order_id': instance.order_delivery.sale_order_id,
                     'delivery_sub_id': instance.id,
                     'product_id': item.get('product_id', None),
                     'actual_value': so_product_cost.product_cost_price * item.get('done', 0),
