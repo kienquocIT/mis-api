407ec39c81b90514f19f05dc0e3a50b7dca64542
trieunt <trieunt@mtsolution.com.vn>
2024-04-05 15:15:35 +0700

    [SIT] update view for import hr.

--- apps/core/hr/views/fimport.py
+++ apps/core/hr/views/fimport.py
@@ -1,9 +1,10 @@
 from drf_yasg.utils import swagger_auto_schema
 
-from apps.core.hr.models import GroupLevel, Group
+from apps.core.hr.models import GroupLevel, Group, Role, Employee
 from apps.core.hr.serializers.fimport import (
     GroupImportSerializer, GroupLevelImportReturnSerializer,
-    GroupImportReturnSerializer, GroupLevelImportSerializer,
+    GroupImportReturnSerializer, GroupLevelImportSerializer, RoleImportReturnSerializer, RoleImportSerializer,
+    EmployeeImportSerializer, EmployeeImportReturnSerializer,
 )
 from apps.shared import BaseCreateMixin, mask_view
 
@@ -48,4 +49,39 @@ class GroupImport(BaseCreateMixin):
         label_code='hr', model_code='group', perm_code='create',
     )
     def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class RoleImport(BaseCreateMixin):
+    queryset = Role.objects
+    serializer_create = RoleImportSerializer
+    serializer_detail = RoleImportReturnSerializer
+    create_hidden_field = ['tenant_id', 'company_id']
+
+    @swagger_auto_schema(operation_summary="Import Role", request_body=RoleImportSerializer)
+    @mask_view(
+        login_require=True, auth_require=True,
+        allow_admin_company=True,
+        label_code='hr', model_code='role', perm_code='create',
+    )
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class EmployeeImport(BaseCreateMixin):
+    queryset = Employee.objects
+    serializer_detail = EmployeeImportReturnSerializer
+    serializer_create = EmployeeImportSerializer
+    create_hidden_field = ('tenant_id', 'company_id')
+
+    @swagger_auto_schema(operation_summary="Import employee", request_body=EmployeeImportSerializer)
+    @mask_view(
+        login_require=True, auth_require=True,
+        allow_admin_tenant=True,
+        label_code='hr', model_code='employee', perm_code='create',
+    )
+    def post(self, request, *args, **kwargs):
+        self.ser_context = {
+            'company_obj': request.user.company_current
+        }
         return self.create(request, *args, **kwargs)
--- apps/core/urls_import.py
+++ apps/core/urls_import.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from apps.core.hr.views.fimport import GroupLevelImport, GroupImport
+from apps.core.hr.views.fimport import (
+    GroupLevelImport, GroupImport, RoleImport, EmployeeImport,
+)
 
 urlpatterns = [
     path('hr/group-level', GroupLevelImport.as_view(), name='GroupLevelImport'),
     path('hr/group', GroupImport.as_view(), name='GroupImport'),
+    path('hr/role', RoleImport.as_view(), name='RoleImport'),
+    path('hr/employee', EmployeeImport.as_view(), name='EmployeeImport'),
 ]
