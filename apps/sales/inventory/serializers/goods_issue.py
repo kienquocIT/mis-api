580c611bcca5d6155c7e543686046fb3ff965f3c
haind <haind@mtsolution.com.vn>
2024-04-21 23:19:08 +0700

    [ft-431] update state for IA after create Goods Issue

--- apps/sales/inventory/serializers/goods_issue.py
+++ apps/sales/inventory/serializers/goods_issue.py
@@ -303,6 +303,7 @@ class GoodsIssueCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         instance = GoodsIssue.objects.create(**validated_data, system_status=3)
         self.common_create_sub_goods_issue(instance, validated_data['goods_issue_datas'])
+        instance.inventory_adjustment.update_ia_state()
         self.prepare_data_for_logging(instance)
         return instance
 
