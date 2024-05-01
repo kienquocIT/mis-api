6d290069d3da167f17f6ed66a6e7e63c676e540e
haind <haind@mtsolution.com.vn>
2024-05-01 17:37:55 +0700

    [432] Update Goods transfer

--- apps/sales/inventory/views/goods_transfer.py
+++ apps/sales/inventory/views/goods_transfer.py
@@ -15,7 +15,6 @@ class GoodsTransferList(BaseListMixin, BaseCreateMixin):
     list_hidden_field = BaseListMixin.LIST_HIDDEN_FIELD_DEFAULT
     create_hidden_field = BaseCreateMixin.CREATE_HIDDEN_FIELD_DEFAULT
 
-
     @swagger_auto_schema(
         operation_summary="Goods transfer List",
         operation_description="Get Goods transfer List",
