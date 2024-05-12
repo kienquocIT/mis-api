f93c63f674c737f1e35f26b4d186fa1f64c468d1
phuclh <phuclh@mtsolution.com.vn>
2024-05-12 18:26:51 +0700

    [feature/ms-274] update function push_diagram PO, GR

--- apps/sales/inventory/utils/logical_gr.py
+++ apps/sales/inventory/utils/logical_gr.py
@@ -4,6 +4,24 @@ from apps.core.diagram.models import DiagramSuffix
 class GRHandler:
     @classmethod
     def push_diagram(cls, instance):
+        data_push = {}
+        for gr_pr_product in instance.goods_receipt_request_product_goods_receipt.all():
+            if gr_pr_product.purchase_request_product and gr_pr_product.goods_receipt_product:
+                if gr_pr_product.purchase_request_product.purchase_request:
+                    if gr_pr_product.purchase_request_product.purchase_request.sale_order:
+                        so_id = gr_pr_product.purchase_request_product.purchase_request.sale_order_id
+                        quantity = gr_pr_product.quantity_import
+                        total = gr_pr_product.goods_receipt_product.product_unit_price * quantity
+                        if str(so_id) not in data_push:
+                            data_push.update({
+                                str(gr_pr_product.purchase_request_product.purchase_request.sale_order_id): {
+                                    'quantity': quantity,
+                                    'total': total,
+                                }
+                            })
+                        else:
+                            data_push[str(so_id)]['quantity'] += quantity
+                            data_push[str(so_id)]['total'] += total
         for purchase_request in instance.purchase_requests.all():
             if purchase_request.sale_order:
                 DiagramSuffix.push_diagram_suffix(
@@ -19,6 +37,9 @@ class GRHandler:
                         'code': instance.code,
                         'system_status': instance.system_status,
                         'date_created': str(instance.date_created),
+                        # custom
+                        'quantity': data_push.get(str(purchase_request.sale_order_id), {}).get('quantity', 0),
+                        'total': data_push.get(str(purchase_request.sale_order_id), {}).get('total', 0),
                     }
                 )
         return True
