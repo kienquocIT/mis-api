c9ebb81b63e633e90acf90e35c3fc8abbda1d3e0
phuclh <phuclh@mtsolution.com.vn>
2024-05-27 15:35:03 +0700

    [feature/ms-274] them search fields BG, DH

--- apps/sales/quotation/views.py
+++ apps/sales/quotation/views.py
@@ -18,7 +18,7 @@ from apps.shared import BaseListMixin, mask_view, BaseCreateMixin, BaseRetrieveM
 
 class QuotationList(BaseListMixin, BaseCreateMixin):
     queryset = Quotation.objects
-    search_fields = ['title', 'code']
+    search_fields = ['title', 'code', 'customer__name']
     filterset_fields = {
         'opportunity': ['exact', 'isnull'],
         'employee_inherit': ['exact'],
--- apps/sales/saleorder/views.py
+++ apps/sales/saleorder/views.py
@@ -18,7 +18,7 @@ from apps.shared import BaseListMixin, mask_view, BaseCreateMixin, BaseRetrieveM
 
 class SaleOrderList(BaseListMixin, BaseCreateMixin):
     queryset = SaleOrder.objects
-    search_fields = ['title', 'code']
+    search_fields = ['title', 'code', 'customer__name']
     filterset_fields = {
         'delivery_call': ['exact'],
         'system_status': ['in'],
