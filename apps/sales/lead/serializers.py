a103c3d0b853e749f6ba22b0e3601820b6fc3edc
haind <haind@mtsolution.com.vn>
2024-05-31 09:57:19 +0700

    [432]: Optimize inventory report

--- apps/sales/lead/serializers.py
+++ apps/sales/lead/serializers.py
@@ -211,11 +211,11 @@ class LeadDetailSerializer(serializers.ModelSerializer):
         all_lead = Lead.objects.filter(tenant=obj.tenant, company=obj.company).exclude(id=obj.id)
         existed = []
         for lead in all_lead:
-            filer_by_contact_name = all_lead.filter(contact_name__icontains=lead.contact_name).count()
-            filer_by_mobile = all_lead.filter(mobile__icontains=lead.mobile).count()
-            filer_by_email = all_lead.filter(email__icontains=lead.email).count()
-            filer_by_company_name = all_lead.filter(company_name__icontains=lead.company_name).count()
-            if sum([filer_by_contact_name, filer_by_mobile, filer_by_email, filer_by_company_name]) > 0:
+            filter_by_contact_name = all_lead.filter(contact_name__icontains=lead.contact_name).count()
+            filter_by_mobile = all_lead.filter(mobile__icontains=lead.mobile).count()
+            filter_by_email = all_lead.filter(email__icontains=lead.email).count()
+            filter_by_company_name = all_lead.filter(company_name__icontains=lead.company_name).count()
+            if sum([filter_by_contact_name, filter_by_mobile, filter_by_email, filter_by_company_name]) > 0:
                 if str(lead.id) not in existed:
                     related_leads.append({
                         'id': lead.id, 'code': lead.code, 'title': lead.title
