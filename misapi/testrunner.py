a32d29a70d689d3c9f553f894f71f3d959f4d327
trieunt <trieunt@mtsolution.com.vn>
2024-07-31 10:06:04 +0700

    [SIT] Update runner skip load init_data + document build and setup cicd environment.

--- misapi/testrunner.py
+++ misapi/testrunner.py
@@ -21,8 +21,7 @@ class CustomTestRunner(DiscoverRunner):
         Step 3: Return _db
         """
         _db = super().setup_databases(**kwargs)
-        if not(settings.CICD_ENABLED__USE_DB_MOCKUP is True and settings.DB_SQLITE_MOCKUP is True):
-            call_command('init_data', **{'limit_wards': True})
+        call_command('init_data', **{'limit_wards': True})
         return _db
 
     def teardown_test_environment(self):
