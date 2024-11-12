e8dafdf94694d028db608f53920b4684de113ff4
namviet <namvietnguyenn@gmail.com>
2024-11-12 10:12:34 +0700

    [feature/ms-003] fix pylint

--- apps/shared/translations/bidding.py
+++ apps/shared/translations/bidding.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class BiddingMsg:
-   ATTACHMENT_REQUIRED = _('Documents must have attachment')
-   BID_SECURITY_TYPE_REQUIRED = _('If there is a bond value, must choose a security type')
-   CAUSE_OF_LOST_REQUIRED = _('If bid status is lost, must choose at least one cause of lost')
-   OTHER_REASON_REQUIRED = _('If cause of lost is "other reason", must specify the reason')
+    ATTACHMENT_REQUIRED = _('Documents must have attachment')
+    BID_SECURITY_TYPE_REQUIRED = _('If there is a bond value, must choose a security type')
+    CAUSE_OF_LOST_REQUIRED = _('If bid status is lost, must choose at least one cause of lost')
+    OTHER_REASON_REQUIRED = _('If cause of lost is "other reason", must specify the reason')
