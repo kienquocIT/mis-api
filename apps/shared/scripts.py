1624dc864f64a810d76b505de94f5c234be25322
phuclh <phuclh@mtsolution.com.vn>
2024-11-21 16:44:06 +0700

    [feature/ms-274] them script reset_remain_gr_for_po()

--- apps/shared/scripts.py
+++ apps/shared/scripts.py
@@ -2534,3 +2534,13 @@ def move_opp_mapped_2_opp():
 
         print(f"Done for {company.title}")
     print('Done :))')
+
+
+def reset_remain_gr_for_po():
+    for pr_product in PurchaseOrderRequestProduct.objects.filter(
+            purchase_order_id="c9e2df0f-227d-4ee1-9b9e-6ba486724b02"
+    ):
+        pr_product.gr_remain_quantity = pr_product.quantity_order
+        pr_product.save(update_fields=['gr_remain_quantity'])
+    print("reset_remain_gr_for_po done.")
+    return True
