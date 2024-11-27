60304e48f1a3beeeb575b35ac120c5085e124915
quannm <quannm@mtsolution.com.vn>
2024-11-27 09:36:08 +0700

    [feature/ms-475][HRM] + commit app HRM

--- apps/hrm/employeeinfo/urls.py
+++ apps/hrm/employeeinfo/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from apps.hrm.employeeinfo.views import EmployeeInfoList, EmployeeInfoDetail, EmployeeNotMapHRMList
+from .views import EmployeeInfoList, EmployeeInfoDetail, EmployeeNotMapHRMList, \
+    EmployeeContractList, EmployeeContractDetail
 
 urlpatterns = [
     path('employee-not-map/list', EmployeeNotMapHRMList.as_view(), name='EmployeeNotMapHRMList'),
     path('employee-info/list', EmployeeInfoList.as_view(), name='EmployeeInfoList'),
     path('employee-info/detail/<str:pk>', EmployeeInfoDetail.as_view(), name='EmployeeInfoDetail'),
+    # contract
+    path('employee-info/contract/list', EmployeeContractList.as_view(), name='EmployeeContractList'),
+    path('employee-info/contract/detail/<str:pk>', EmployeeContractDetail.as_view(), name='EmployeeContractDetail'),
 ]
