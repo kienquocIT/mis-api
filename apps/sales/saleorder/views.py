53e4a32a00433a42071822be7509411d45894ac2
phuclh <phuclh@mtsolution.com.vn>
2024-11-28 12:47:14 +0700

    [feature/ms-274] them model abstract Recurrence cho model ARInvoice

--- apps/sales/saleorder/views.py
+++ apps/sales/saleorder/views.py
@@ -324,7 +324,7 @@ class SORecurrenceList(BaseListMixin, BaseCreateMixin):
 
     @swagger_auto_schema(
         operation_summary="SO Recurrence List",
-        operation_description="Get SO SORecurrence List",
+        operation_description="Get SO Recurrence List",
     )
     @mask_view(login_require=True, auth_require=False)
     def get(self, request, *args, **kwargs):
