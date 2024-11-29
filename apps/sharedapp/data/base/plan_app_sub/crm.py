8310ce9f0783caa9254acaaf52d4dc647d5533fe
namviet <namvietnguyenn@gmail.com>
2024-11-29 14:49:02 +0700

    [feature/ms-004] add process for bidding

--- apps/sharedapp/data/base/plan_app_sub/crm.py
+++ apps/sharedapp/data/base/plan_app_sub/crm.py
@@ -2348,6 +2348,7 @@ BIDDING_APP_CONFIG = {
     },
     "allow_permit": True,
     "allow_print": True,
+    "allow_process": True,
 }
 
 # Nhóm 1: các chức năng quản lý phân quyền theo space opportunity
