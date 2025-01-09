f39e5c54726e145c7f0789e48d3fb50a2a461e6d
trieunt <trieunt@mtsolution.com.vn>
2025-01-09 15:33:19 +0700

    [SIT] Fix pylint.

--- f_push.py
+++ f_push.py
@@ -1,12 +1,12 @@
-from firebase_admin.messaging import Message, Notification, send
-
-
-def send_fcm_notification(token, title, body):
-    message = Message(
-        token=token,
-        notification=Notification(
-            title=title,
-            body=body,
-        ),
-    )
-    send(message)
+# from firebase_admin.messaging import Message, Notification, send
+#
+#
+# def send_fcm_notification(token, title, body):
+#     message = Message(
+#         token=token,
+#         notification=Notification(
+#             title=title,
+#             body=body,
+#         ),
+#     )
+#     send(message)
