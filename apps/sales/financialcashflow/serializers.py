93c87f3ea26ecc3103d3b39098785d63ca1011f5
haind <haind@mtsolution.com.vn>
2025-02-06 23:54:34 +0700

    [accounting/ft/ms-438]: accounting masterdata + Cashinflow

--- apps/sales/financialcashflow/serializers.py
+++ apps/sales/financialcashflow/serializers.py
@@ -473,7 +473,6 @@ class ARInvoiceListForCashInflowSerializer(serializers.ModelSerializer):
         # payment_value = sum(item.sum_payment_value for item in obj.cash_inflow_item_ar_invoice.filter(
         #     cash_inflow__system_status=3
         # ))
-        print(obj.id)
         payment_value = sum(item.recon_amount for item in obj.recon_item_ar_invoice.all())
         return payment_value
 
