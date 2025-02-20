7b090eb98c19ce8d966cb1a9dc7c043ea680f853
haind <haind@mtsolution.com.vn>
2025-02-21 04:59:54 +0700

    [act/ft/448]: Journal Entry

--- apps/masterdata/saledata/models/periods.py
+++ apps/masterdata/saledata/models/periods.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
@@ -12,22 +13,6 @@ DEFINITION_INVENTORY_VALUATION_CHOICES = [
     (1, _('Periodic inventory')),
 ]
 
-DEFAULT_INVENTORY_VALUE_METHOD_CHOICES = [
-    (0, _('FIFO')),
-    (1, _('Weighted average')),
-    (2, _('Specific identification method')),
-]
-
-ACCOUNTING_POLICIES_CHOICES = [
-    (0, _('VAS')),
-    (1, _('IAS')),
-]
-
-APPLICABLE_CIRCULAR_CHOICES = [
-    (0, '200/2014/TT-BTC'),
-    (1, '133/2015/TT-BTC'),
-]
-
 
 class Periods(MasterDataAbstractModel):
     space_month = models.IntegerField(default=0)
@@ -37,14 +22,7 @@ class Periods(MasterDataAbstractModel):
     has_revenue_planned = models.BooleanField(default=False, help_text=_('is True if has revenue planned this period'))
     has_budget_planned = models.BooleanField(default=False, help_text=_('is True if has budget planned this period'))
     sub_periods_type = models.IntegerField(choices=[(0, 'Month'), (1, 'Quarter'), (2, 'Year')], default=0)
-
     definition_inventory_valuation = models.SmallIntegerField(choices=DEFINITION_INVENTORY_VALUATION_CHOICES, default=0)
-    default_inventory_value_method = models.SmallIntegerField(choices=DEFAULT_INVENTORY_VALUE_METHOD_CHOICES, default=1)
-    cost_per_warehouse = models.BooleanField(default=True)
-    cost_per_lot = models.BooleanField(default=False)
-    cost_per_project = models.BooleanField(default=False)
-    accounting_policies = models.SmallIntegerField(choices=ACCOUNTING_POLICIES_CHOICES, default=0)
-    applicable_circular = models.SmallIntegerField(choices=APPLICABLE_CIRCULAR_CHOICES, default=0)
 
     @classmethod
     def get_period_by_doc_date(cls, tenant_id, company_id, doc_date):
@@ -108,30 +86,33 @@ class SubPeriods(SimpleAbstractModel):
     @classmethod
     def check_period(cls, tenant_id, company_id):
         company_obj = Company.objects.filter(id=company_id).first()
-        if company_obj:
-            software_start_using_time = company_obj.software_start_using_time
-            if software_start_using_time:
-                if software_start_using_time.date() > datetime.now().date():
-                    raise serializers.ValidationError(
-                        {"Error": f'[check_period] Can not create an inventory activity before Software start using time'
-                                  f' ({software_start_using_time.date()})'}
-                    )
-            else:
-                raise serializers.ValidationError({"Error": '[check_period] Software start using time is not found.'})
-        else:
-            raise serializers.ValidationError({"Error": '[check_period] Company is not found.'})
+        if not company_obj:
+            raise serializers.ValidationError({"Error": "[check_period] Company is not found."})
+
+        software_start_using_time = company_obj.software_start_using_time
+        if not software_start_using_time:
+            raise serializers.ValidationError({"Error": "[check_period] Software start using time is not found."})
+
+        if software_start_using_time.date() > timezone.now().date():
+            raise serializers.ValidationError({
+                "Error": f"[check_period] Cannot create an inventory activity before Software start using time "
+                         f"({software_start_using_time.date()})"
+            })
+
         this_period = Periods.get_current_period(tenant_id, company_id)
-        if this_period:
-            this_sub_period = Periods.get_current_sub_period(this_period)
-            if this_sub_period:
-                if this_sub_period.locked:
-                    raise serializers.ValidationError(
-                        {"Error": '[check_period] Can not create an inventory activity now. '
-                                  'This sub period has been Locked.'}
-                    )
-                return True
-            raise serializers.ValidationError({"Error": '[check_period] This sub is not found.'})
-        raise serializers.ValidationError({"Error": '[check_period] This period is not found.'})
+        if not this_period:
+            raise serializers.ValidationError({"Error": "[check_period] This period is not found."})
+
+        this_sub_period = Periods.get_current_sub_period(this_period)
+        if not this_sub_period:
+            raise serializers.ValidationError({"Error": "[check_period] This sub period is not found."})
+
+        if this_sub_period.locked:
+            raise serializers.ValidationError({
+                "Error": "[check_period] Cannot create an inventory activity now. This sub period has been Locked."
+            })
+
+        return True
 
     class Meta:
         verbose_name = 'Subs Period'
