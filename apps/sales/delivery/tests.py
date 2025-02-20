bf561ea844b9f71e1067c4a2d05ba8ad6c2d4ee4
haind <haind@mtsolution.com.vn>
2025-02-21 05:32:26 +0700

    [hotfix/haind3]: update period

--- apps/sales/delivery/tests.py
+++ apps/sales/delivery/tests.py
@@ -35,13 +35,14 @@ class PickingDeliveryTestCase(AdvanceTestCase):
             'address': '7826 avenue, Victoria Street, California, American',
             'email': 'mike.nguyen.7826@gmail.com',
             'phone': '0983875345',
-            'primary_currency': self.get_base_currency().data['result'][0]['id'],
-            'software_start_using_time': timezone.now().replace(
-                month=1, day=1, hour=0, minute=0, second=0, microsecond=0
-            )
+            'primary_currency': self.get_base_currency().data['result'][0]['id']
         }
         company_req = self.client.post(reverse("CompanyList"), company_data, format='json')
         self.company = company_req
+        self.company.software_start_using_time = timezone.now().replace(
+            month=1, day=1, hour=0, minute=0, second=0, microsecond=0
+        )
+        self.company.save(update_fields=['software_start_using_time'])
         config = DeliveryConfig.objects.get_or_create(
             company_id=company_req.data['result']['id'],
             defaults={
