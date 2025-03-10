91a597e4869534a904539501f509290708577325
namviet <vietnn@mtsolution.com>
2025-03-10 15:56:08 +0700

    [feature/ms-007] create bank and bank account model

---
+++ apps/masterdata/saledata/models/bank.py
@@ -0,0 +1,91 @@
+from django.db import models
+from apps.shared import MasterDataAbstractModel
+
+__all__ = [
+    'Bank',
+    'BankAccount'
+]
+
+class Bank(MasterDataAbstractModel):
+    abbreviation = models.CharField(max_length=150)
+    vietnamese_name = models.CharField(max_length=150)
+    english_name = models.CharField(max_length=150)
+    is_default = models.BooleanField(default=False)
+
+    country = models.ForeignKey(
+        'base.Country',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='country_banks'
+    )
+    city = models.ForeignKey(
+        'base.City',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='city_banks'
+    )
+    district = models.ForeignKey(
+        'base.District',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='district_banks'
+    )
+    ward = models.ForeignKey(
+        'base.Ward',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='ward_banks'
+    )
+    address = models.TextField(null=True)
+    full_address = models.TextField(null=True)
+
+    class Meta:
+        verbose_name = 'Bank'
+        verbose_name_plural = 'Bank'
+        ordering = ('code',)
+
+class BankAccount(MasterDataAbstractModel):
+    bank_abbreviation = models.ForeignKey(
+        'Bank',
+        on_delete=models.CASCADE,
+        related_name='bank_accounts'
+    )
+    bank_name = models.CharField(max_length=150)
+    bank_account_number = models.CharField(max_length=150)
+    bank_owner = models.CharField(max_length=150)
+    is_brand = models.BooleanField(default=False)
+    brand_name = models.CharField(max_length=150, null=True)
+    is_default = models.BooleanField(default=False)
+
+    brand_country = models.ForeignKey(
+        'base.Country',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='country_bank_accounts'
+    )
+    brand_city = models.ForeignKey(
+        'base.City',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='city_bank_accounts'
+    )
+    brand_district = models.ForeignKey(
+        'base.District',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='district_bank_accounts'
+    )
+    brand_ward = models.ForeignKey(
+        'base.Ward',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='ward_bank_accounts'
+    )
+    brand_address = models.TextField(null=True)
+    brand_full_address = models.TextField(null=True)
+    head_address = models.TextField(null=True)
+
+    class Meta:
+        verbose_name = 'BankAccount'
+        verbose_name_plural = 'BankAccountS'
+        ordering = ('code',)
