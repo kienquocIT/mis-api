b82b34b383714c7d2ea46d0c1736037b91ace370
namviet <vietnn@mtsolution.com>
2025-03-26 09:28:59 +0700

    [feature/ms-007] group order

--- apps/sales/grouporder/urls.py
+++ apps/sales/grouporder/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import GroupOrderProductList, GroupOrderProductPriceListList
+from .views import GroupOrderProductList, GroupOrderProductPriceListList, GroupOrderList, GroupOrderDetail
 
 urlpatterns = [
     path('product/list', GroupOrderProductList.as_view(), name='GroupOrderProductList'),
@@ -9,4 +9,6 @@ urlpatterns = [
         GroupOrderProductPriceListList.as_view(),
         name='GroupOrderProductPriceListList'
     ),
+    path( 'list', GroupOrderList.as_view(), name='GroupOrderList'),
+    path('detail/<str:pk>', GroupOrderDetail.as_view(), name='GroupOrderDetail'),
 ]
--- apps/sales/grouporder/views.py
+++ apps/sales/grouporder/views.py
@@ -2,7 +2,9 @@ from django.db.models import Prefetch
 from drf_yasg.utils import swagger_auto_schema
 
 from apps.masterdata.saledata.models import Product, ProductPriceList
-from apps.sales.grouporder.serializers import GroupOrderProductListSerializer, GroupOrderProductPriceListListSerializer
+from apps.sales.grouporder.models import GroupOrder
+from apps.sales.grouporder.serializers import GroupOrderProductListSerializer, GroupOrderProductPriceListListSerializer, \
+    GroupOrderCreateSerializer, GroupOrderListSerializer, GroupOrderDetailSerializer, GroupOrderUpdateSerializer
 from apps.shared import (
     mask_view, ResponseController,
     BaseListMixin, BaseCreateMixin, BaseRetrieveMixin, BaseUpdateMixin, BaseDestroyMixin,
@@ -53,3 +55,73 @@ class GroupOrderProductPriceListList(BaseListMixin, BaseCreateMixin):
     )
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+
+class GroupOrderList(BaseListMixin, BaseCreateMixin):
+    queryset = GroupOrder.objects
+    search_fields = ['title', 'code']
+    filterset_fields = {}
+    serializer_list = GroupOrderListSerializer
+    serializer_create = GroupOrderCreateSerializer
+    serializer_detail = GroupOrderDetailSerializer
+    list_hidden_field = BaseListMixin.LIST_HIDDEN_FIELD_DEFAULT
+    create_hidden_field = [
+        'tenant_id', 'company_id',
+        'employee_created_id',
+    ]
+
+    @swagger_auto_schema(
+        operation_summary="Group Order List",
+        operation_description="Get Group Order List",
+    )
+    @mask_view(
+        login_require=True, auth_require=False,
+        label_code='grouporder', model_code='grouporder', perm_code='view',
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Create Group Order",
+        operation_description="Create New Group Order",
+        request_body=GroupOrderCreateSerializer,
+    )
+    @mask_view(
+        login_require=True, auth_require=False,
+        label_code='grouporder', model_code='grouporder', perm_code='create',
+    )
+    def post(self, request, *args, **kwargs):
+        self.ser_context = {'user': request.user}
+        return self.create(request, *args, **kwargs)
+
+
+class GroupOrderDetail(BaseRetrieveMixin, BaseUpdateMixin):
+    queryset = GroupOrder.objects
+    serializer_detail = GroupOrderDetailSerializer
+    serializer_update = GroupOrderUpdateSerializer
+    retrieve_hidden_field = BaseRetrieveMixin.RETRIEVE_HIDDEN_FIELD_DEFAULT
+    update_hidden_field = BaseUpdateMixin.UPDATE_HIDDEN_FIELD_DEFAULT
+
+    @swagger_auto_schema(
+        operation_summary="Group Order Detail",
+        operation_description="Get Group Order Detail",
+    )
+    @mask_view(
+        login_require=True, auth_require=False,
+        label_code='grouporder', model_code='grouporder', perm_code='view',
+    )
+    def get(self, request, *args, pk, **kwargs):
+        return self.retrieve(request, *args, pk, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Group Order Update",
+        operation_description="Group Order Update",
+        request_body=GroupOrderUpdateSerializer,
+    )
+    @mask_view(
+        login_require=True, auth_require=False,
+        label_code='grouporder', model_code='grouporder', perm_code='edit',
+    )
+    def put(self, request, *args, pk, **kwargs):
+        self.ser_context = {'user': request.user}
+        return self.update(request, *args, pk, **kwargs)
