5340828a7a6b56c934563cabdde15d59d50b19ae
haind <haind@mtsolution.com.vn>
2025-04-17 14:08:06 +0700

    [ft/439]: update Company Primary Currency

--- apps/shared/translations/company.py
+++ apps/shared/translations/company.py
@@ -6,7 +6,7 @@ __all__ = ['CompanyMsg']
 class CompanyMsg:
     VALID_NEED_TENANT_DATA = _('Valid company maximum raise errors.')
     MAXIMUM_COMPANY_LIMITED = _('Maximum company is {} companies.')
-    CURRENCY_NOT_EXIST = _("Currency isn't support for company.")
+    CURRENCY_NOT_EXIST = _("Currency either does not exist or unsupported for company.")
     LANGUAGE_NOT_SUPPORT = _("Language isn't support.")
     SUB_DOMAIN_EXIST = _("Sub domain already exists. Please choose another name.")
     INVALID_COMPANY_SETTING_DATA = _("Invalid company setting data.")
