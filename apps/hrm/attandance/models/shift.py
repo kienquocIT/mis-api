21a50938fd9edcd6abad56e382476bfcbaf66f0b
Windows-key <giacanemail@gmail.com>
2025-07-14 11:49:04 +0700

    [feature/ms-243]: comment shift masterdata function to prevent migrate

--- apps/hrm/attandance/models/shift.py
+++ apps/hrm/attandance/models/shift.py
@@ -1,46 +1,46 @@
-from django.db import models
-
-from apps.shared import MasterDataAbstractModel
-
-
-class ShiftInfo(MasterDataAbstractModel):
-    checkin_time = models.TimeField(verbose_name='The start time')
-    checkin_gr_start = models.TimeField()
-    checkin_gr_end = models.TimeField()
-    checkin_threshold = models.FloatField()
-
-    break_in_time = models.TimeField(verbose_name='The break start time')
-    break_in_gr_start = models.TimeField()
-    break_in_gr_end = models.TimeField()
-    break_in_threshold = models.FloatField()
-
-    break_out_time = models.TimeField(verbose_name='The break end time')
-    break_out_gr_start = models.TimeField()
-    break_out_gr_end = models.TimeField()
-    break_out_threshold = models.FloatField()
-
-    checkout_time = models.TimeField(verbose_name='The end time')
-    checkout_gr_start = models.TimeField()
-    checkout_gr_end = models.TimeField()
-    checkout_threshold = models.FloatField()
-
-    working_day_list = models.JSONField(default=dict)
-    # working_day_list = {
-    #   'Mon': True,
-    #   'Tue': False,
-    #   ...
-    # }
-    description = models.TextField(blank=True, null=True)
-
-    class Meta:
-        verbose_name = 'Shift information'
-        verbose_name_plural = 'Shift information'
-        ordering = ('-date_created',)
-        default_permissions = ()
-        permissions = ()
-
-    def save(self, *args, **kwargs):
-        if not self.code:
-            count = ShiftInfo.objects.filter_on_company().count() + 1
-            self.code = f'SH00{count}'
-        super().save(*args, **kwargs)
+# from django.db import models
+#
+# from apps.shared import MasterDataAbstractModel
+#
+#
+# class ShiftInfo(MasterDataAbstractModel):
+#     checkin_time = models.TimeField(verbose_name='The start time')
+#     checkin_gr_start = models.TimeField()
+#     checkin_gr_end = models.TimeField()
+#     checkin_threshold = models.FloatField()
+#
+#     break_in_time = models.TimeField(verbose_name='The break start time')
+#     break_in_gr_start = models.TimeField()
+#     break_in_gr_end = models.TimeField()
+#     break_in_threshold = models.FloatField()
+#
+#     break_out_time = models.TimeField(verbose_name='The break end time')
+#     break_out_gr_start = models.TimeField()
+#     break_out_gr_end = models.TimeField()
+#     break_out_threshold = models.FloatField()
+#
+#     checkout_time = models.TimeField(verbose_name='The end time')
+#     checkout_gr_start = models.TimeField()
+#     checkout_gr_end = models.TimeField()
+#     checkout_threshold = models.FloatField()
+#
+#     working_day_list = models.JSONField(default=dict)
+#     # working_day_list = {
+#     #   'Mon': True,
+#     #   'Tue': False,
+#     #   ...
+#     # }
+#     description = models.TextField(blank=True, null=True)
+#
+#     class Meta:
+#         verbose_name = 'Shift information'
+#         verbose_name_plural = 'Shift information'
+#         ordering = ('-date_created',)
+#         default_permissions = ()
+#         permissions = ()
+#
+#     def save(self, *args, **kwargs):
+#         if not self.code:
+#             count = ShiftInfo.objects.filter_on_company().count() + 1
+#             self.code = f'SH00{count}'
+#         super().save(*args, **kwargs)
--- apps/hrm/attandance/serializers/shift.py
+++ apps/hrm/attandance/serializers/shift.py
@@ -1,222 +1,222 @@
-from datetime import datetime, time
-
-from rest_framework import serializers
-
-from apps.hrm.attandance.models import ShiftInfo
-from apps.shared.translations.hrm import HRMMsg
-
-
-class CommonFunction:
-    @staticmethod
-    def validate_time_fields(validate_data):
-        def is_valid_time(value):
-            if isinstance(value, time):
-                return True
-            if isinstance(value, str):
-                try:
-                    datetime.strptime(value, "%H:%M")
-                    return True
-                except ValueError:
-                    return False
-            return False
-
-        time_fields = {
-            'checkin_time': HRMMsg.VALIDATE_TIME_FORMAT_ERROR,
-            'break_in_time': HRMMsg.VALIDATE_TIME_FORMAT_ERROR,
-            'break_out_time': HRMMsg.VALIDATE_TIME_FORMAT_ERROR,
-            'checkout_time': HRMMsg.VALIDATE_TIME_FORMAT_ERROR,
-        }
-
-        for field, error_msg in time_fields.items():
-            value = validate_data.get(field)
-            if value and not is_valid_time(value):
-                raise serializers.ValidationError({field: error_msg})
-
-        return validate_data
-
-    @staticmethod
-    def validate_shift_time_order(validate_data):
-        checkin_time = validate_data.get('checkin_time', '')
-        break_in_time = validate_data.get('break_in_time', '')
-        break_out_time = validate_data.get('break_out_time', '')
-        checkout_time = validate_data.get('checkout_time', '')
-
-        # validate break in must be later than check in time
-        if checkin_time and break_in_time and checkin_time >= break_in_time:
-            raise serializers.ValidationError({
-                'break_in_time': HRMMsg.BREAK_IN_TIME_ERROR
-            })
-
-        # validate break out must be later than break in time
-        if break_out_time and break_in_time and break_in_time >= break_out_time:
-            raise serializers.ValidationError({
-                'break_out_time': HRMMsg.BREAK_OUT_TIME_ERROR
-            })
-
-        # validate check out must be later than break out time
-        if checkout_time and break_out_time and break_out_time >= checkout_time:
-            raise serializers.ValidationError({
-                'checkout_time': HRMMsg.CHECKOUT_TIME_ERROR
-            })
-        return validate_data
-
-
-class ShiftListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ShiftInfo
-        fields = (
-            'id',
-            'code',
-            'title',
-            'description',
-            'checkin_time',
-            'checkin_gr_start',
-            'checkin_gr_end',
-            'checkin_threshold',
-            'break_in_time',
-            'break_in_gr_start',
-            'break_in_gr_end',
-            'break_in_threshold',
-            'break_out_time',
-            'break_out_gr_start',
-            'break_out_gr_end',
-            'break_out_threshold',
-            'checkout_time',
-            'checkout_gr_start',
-            'checkout_gr_end',
-            'checkout_threshold',
-            'working_day_list',
-        )
-
-
-class ShiftCreateSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length=100)
-
-    def validate(self, validate_data):
-        # checkin_time = validate_data.get('checkin_time', '')
-        # break_in_time = validate_data.get('break_in_time', '')
-        # break_out_time = validate_data.get('break_out_time', '')
-        # checkout_time = validate_data.get('checkout_time', '')
-        #
-        # # validate break in must be later than check in time
-        # if checkin_time and break_in_time and checkin_time >= break_in_time:
-        #     raise serializers.ValidationError({
-        #         'break_in_time': HRMMsg.BREAK_IN_TIME_ERROR
-        #     })
-        #
-        # # validate break out must be later than break in time
-        # if break_out_time and break_in_time and break_in_time >= break_out_time:
-        #     raise serializers.ValidationError({
-        #         'break_out_time': HRMMsg.BREAK_OUT_TIME_ERROR
-        #     })
-        #
-        # # validate check out must be later than break out time
-        # if checkout_time and break_out_time and break_out_time >= checkout_time:
-        #     raise serializers.ValidationError({
-        #         'checkout_time': HRMMsg.CHECKOUT_TIME_ERROR
-        #     })
-        # return validate_data
-        CommonFunction.validate_time_fields(validate_data)
-        CommonFunction.validate_shift_time_order(validate_data)
-
-    def create(self, validated_data):
-        checkin_time_str = validated_data.get('checkin_time', '').strftime('%H:%M')
-        checkout_time_str = validated_data.get('checkout_time', '').strftime('%H:%M')
-        working_days = validated_data.get('working_day_list', [])
-        active_days = [key for key, val in working_days.items() if val] if working_days else []
-        description = f'{checkin_time_str} - {checkout_time_str} {", ".join(active_days)}'
-        return ShiftInfo.objects.create(**validated_data, description=description)
-
-    class Meta:
-        model = ShiftInfo
-        fields = (
-            'title',
-            'checkin_time',
-            'checkin_gr_start',
-            'checkin_gr_end',
-            'checkin_threshold',
-            'break_in_time',
-            'break_in_gr_start',
-            'break_in_gr_end',
-            'break_in_threshold',
-            'break_out_time',
-            'break_out_gr_start',
-            'break_out_gr_end',
-            'break_out_threshold',
-            'checkout_time',
-            'checkout_gr_start',
-            'checkout_gr_end',
-            'checkout_threshold',
-            'working_day_list',
-        )
-
-
-class ShiftDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ShiftInfo
-        fields = (
-            'id',
-            'code',
-            'title',
-            'checkin_time',
-            'checkin_gr_start',
-            'checkin_gr_end',
-            'checkin_threshold',
-            'break_in_time',
-            'break_in_gr_start',
-            'break_in_gr_end',
-            'break_in_threshold',
-            'break_out_time',
-            'break_out_gr_start',
-            'break_out_gr_end',
-            'break_out_threshold',
-            'checkout_time',
-            'checkout_gr_start',
-            'checkout_gr_end',
-            'checkout_threshold',
-            'working_day_list',
-        )
-
-
-class ShiftUpdateSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length=100)
-
-    def validate(self, validate_data):
-        return ShiftCreateSerializer().validate(validate_data)
-
-    def update(self, instance, validated_data):
-        checkin_time_str = validated_data.get('checkin_time', '').strftime('%H:%M')
-        checkout_time_str = validated_data.get('checkout_time', '').strftime('%H:%M')
-        working_days = validated_data.get('working_day_list', [])
-        active_days = [key for key, val in working_days.items() if val] if working_days else []
-        description = f'{checkin_time_str} - {checkout_time_str} {", ".join(active_days)}'
-
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        instance.description = description
-        instance.save()
-        return instance
-
-    class Meta:
-        model = ShiftInfo
-        fields = (
-            'title',
-            'checkin_time',
-            'checkin_gr_start',
-            'checkin_gr_end',
-            'checkin_threshold',
-            'break_in_time',
-            'break_in_gr_start',
-            'break_in_gr_end',
-            'break_in_threshold',
-            'break_out_time',
-            'break_out_gr_start',
-            'break_out_gr_end',
-            'break_out_threshold',
-            'checkout_time',
-            'checkout_gr_start',
-            'checkout_gr_end',
-            'checkout_threshold',
-            'working_day_list',
-        )
+# from datetime import datetime, time
+#
+# from rest_framework import serializers
+#
+# from apps.hrm.attandance.models.shift import ShiftInfo
+# from apps.shared.translations.hrm import HRMMsg
+#
+#
+# class CommonFunction:
+#     @staticmethod
+#     def validate_time_fields(validate_data):
+#         def is_valid_time(value):
+#             if isinstance(value, time):
+#                 return True
+#             if isinstance(value, str):
+#                 try:
+#                     datetime.strptime(value, "%H:%M")
+#                     return True
+#                 except ValueError:
+#                     return False
+#             return False
+#
+#         time_fields = {
+#             'checkin_time': HRMMsg.VALIDATE_TIME_FORMAT_ERROR,
+#             'break_in_time': HRMMsg.VALIDATE_TIME_FORMAT_ERROR,
+#             'break_out_time': HRMMsg.VALIDATE_TIME_FORMAT_ERROR,
+#             'checkout_time': HRMMsg.VALIDATE_TIME_FORMAT_ERROR,
+#         }
+#
+#         for field, error_msg in time_fields.items():
+#             value = validate_data.get(field)
+#             if value and not is_valid_time(value):
+#                 raise serializers.ValidationError({field: error_msg})
+#
+#         return validate_data
+#
+#     @staticmethod
+#     def validate_shift_time_order(validate_data):
+#         checkin_time = validate_data.get('checkin_time', '')
+#         break_in_time = validate_data.get('break_in_time', '')
+#         break_out_time = validate_data.get('break_out_time', '')
+#         checkout_time = validate_data.get('checkout_time', '')
+#
+#         # validate break in must be later than check in time
+#         if checkin_time and break_in_time and checkin_time >= break_in_time:
+#             raise serializers.ValidationError({
+#                 'break_in_time': HRMMsg.BREAK_IN_TIME_ERROR
+#             })
+#
+#         # validate break out must be later than break in time
+#         if break_out_time and break_in_time and break_in_time >= break_out_time:
+#             raise serializers.ValidationError({
+#                 'break_out_time': HRMMsg.BREAK_OUT_TIME_ERROR
+#             })
+#
+#         # validate check out must be later than break out time
+#         if checkout_time and break_out_time and break_out_time >= checkout_time:
+#             raise serializers.ValidationError({
+#                 'checkout_time': HRMMsg.CHECKOUT_TIME_ERROR
+#             })
+#         return validate_data
+#
+#
+# class ShiftListSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ShiftInfo
+#         fields = (
+#             'id',
+#             'code',
+#             'title',
+#             'description',
+#             'checkin_time',
+#             'checkin_gr_start',
+#             'checkin_gr_end',
+#             'checkin_threshold',
+#             'break_in_time',
+#             'break_in_gr_start',
+#             'break_in_gr_end',
+#             'break_in_threshold',
+#             'break_out_time',
+#             'break_out_gr_start',
+#             'break_out_gr_end',
+#             'break_out_threshold',
+#             'checkout_time',
+#             'checkout_gr_start',
+#             'checkout_gr_end',
+#             'checkout_threshold',
+#             'working_day_list',
+#         )
+#
+#
+# class ShiftCreateSerializer(serializers.ModelSerializer):
+#     title = serializers.CharField(max_length=100)
+#
+#     def validate(self, validate_data):
+#         # checkin_time = validate_data.get('checkin_time', '')
+#         # break_in_time = validate_data.get('break_in_time', '')
+#         # break_out_time = validate_data.get('break_out_time', '')
+#         # checkout_time = validate_data.get('checkout_time', '')
+#         #
+#         # # validate break in must be later than check in time
+#         # if checkin_time and break_in_time and checkin_time >= break_in_time:
+#         #     raise serializers.ValidationError({
+#         #         'break_in_time': HRMMsg.BREAK_IN_TIME_ERROR
+#         #     })
+#         #
+#         # # validate break out must be later than break in time
+#         # if break_out_time and break_in_time and break_in_time >= break_out_time:
+#         #     raise serializers.ValidationError({
+#         #         'break_out_time': HRMMsg.BREAK_OUT_TIME_ERROR
+#         #     })
+#         #
+#         # # validate check out must be later than break out time
+#         # if checkout_time and break_out_time and break_out_time >= checkout_time:
+#         #     raise serializers.ValidationError({
+#         #         'checkout_time': HRMMsg.CHECKOUT_TIME_ERROR
+#         #     })
+#         # return validate_data
+#         CommonFunction.validate_time_fields(validate_data)
+#         CommonFunction.validate_shift_time_order(validate_data)
+#
+#     def create(self, validated_data):
+#         checkin_time_str = validated_data.get('checkin_time', '').strftime('%H:%M')
+#         checkout_time_str = validated_data.get('checkout_time', '').strftime('%H:%M')
+#         working_days = validated_data.get('working_day_list', [])
+#         active_days = [key for key, val in working_days.items() if val] if working_days else []
+#         description = f'{checkin_time_str} - {checkout_time_str} {", ".join(active_days)}'
+#         return ShiftInfo.objects.create(**validated_data, description=description)
+#
+#     class Meta:
+#         model = ShiftInfo
+#         fields = (
+#             'title',
+#             'checkin_time',
+#             'checkin_gr_start',
+#             'checkin_gr_end',
+#             'checkin_threshold',
+#             'break_in_time',
+#             'break_in_gr_start',
+#             'break_in_gr_end',
+#             'break_in_threshold',
+#             'break_out_time',
+#             'break_out_gr_start',
+#             'break_out_gr_end',
+#             'break_out_threshold',
+#             'checkout_time',
+#             'checkout_gr_start',
+#             'checkout_gr_end',
+#             'checkout_threshold',
+#             'working_day_list',
+#         )
+#
+#
+# class ShiftDetailSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ShiftInfo
+#         fields = (
+#             'id',
+#             'code',
+#             'title',
+#             'checkin_time',
+#             'checkin_gr_start',
+#             'checkin_gr_end',
+#             'checkin_threshold',
+#             'break_in_time',
+#             'break_in_gr_start',
+#             'break_in_gr_end',
+#             'break_in_threshold',
+#             'break_out_time',
+#             'break_out_gr_start',
+#             'break_out_gr_end',
+#             'break_out_threshold',
+#             'checkout_time',
+#             'checkout_gr_start',
+#             'checkout_gr_end',
+#             'checkout_threshold',
+#             'working_day_list',
+#         )
+#
+#
+# class ShiftUpdateSerializer(serializers.ModelSerializer):
+#     title = serializers.CharField(max_length=100)
+#
+#     def validate(self, validate_data):
+#         return ShiftCreateSerializer().validate(validate_data)
+#
+#     def update(self, instance, validated_data):
+#         checkin_time_str = validated_data.get('checkin_time', '').strftime('%H:%M')
+#         checkout_time_str = validated_data.get('checkout_time', '').strftime('%H:%M')
+#         working_days = validated_data.get('working_day_list', [])
+#         active_days = [key for key, val in working_days.items() if val] if working_days else []
+#         description = f'{checkin_time_str} - {checkout_time_str} {", ".join(active_days)}'
+#
+#         for attr, value in validated_data.items():
+#             setattr(instance, attr, value)
+#
+#         instance.description = description
+#         instance.save()
+#         return instance
+#
+#     class Meta:
+#         model = ShiftInfo
+#         fields = (
+#             'title',
+#             'checkin_time',
+#             'checkin_gr_start',
+#             'checkin_gr_end',
+#             'checkin_threshold',
+#             'break_in_time',
+#             'break_in_gr_start',
+#             'break_in_gr_end',
+#             'break_in_threshold',
+#             'break_out_time',
+#             'break_out_gr_start',
+#             'break_out_gr_end',
+#             'break_out_threshold',
+#             'checkout_time',
+#             'checkout_gr_start',
+#             'checkout_gr_end',
+#             'checkout_threshold',
+#             'working_day_list',
+#         )
