80c757c65c4dfce570fbf13a0dcd2bf5f9461bdb
quannm <quannm@mtsolution.com.vn>
2025-08-25 11:06:17 +0700

    [hotfix/quannm][tasks] + add mail for comment and tasks when create new. + add document for create background task send mail.

--- apps/core/mailer/templates.py
+++ apps/core/mailer/templates.py
@@ -1,5 +1,7 @@
 __all__ = ['TEMPLATE_OTP_VALIDATE_DEFAULT', 'TEMPLATE_MAIL_WELCOME_DEFAULT', 'TEMPLATE_CALENDAR_DEFAULT',
-           'TEMPLATE_PROJECT_NEW_DEFAULT', 'TEMPLATE_REQUEST_CONTRACT_DEFAULT', 'TEMPLATE_REQUEST_LEAVE_DEFAULT', ]
+           'TEMPLATE_PROJECT_NEW_DEFAULT', 'TEMPLATE_REQUEST_CONTRACT_DEFAULT', 'TEMPLATE_REQUEST_LEAVE_DEFAULT',
+           'SUBJECT_NEW_COMMENT_DEFAULT', 'TEMPLATE_MENTION_COMMENT_DEFAULT', 'SUBJECT_NEW_TASK_DEFAULT',
+           'TEMPLATE_NEW_TASKS_DEFAULT']
 
 from django.utils.translation import gettext_lazy as _
 
@@ -100,4 +102,22 @@ TEMPLATE_REQUEST_LEAVE_DEFAULT = _(
     "<div><p>Employee <span class=\"params-data\" data-code=\"_leave__employee\"></span> has been granted leave for about <span class=\"params-data\" data-code=\"_leave__day_off\"></span> days, and will return to work on <span class=\"params-data\" data-code=\"_leave__date_back\"></span>.</p>"
     "<p>During this period, please contact <span class=\"params-data\" data-code=\"_leave__leader_name\"></span> at <span class=\"params-data\" data-code=\"_leave__leader_email\"></span> if your email requires immediate attention.</p>"
     "</div>"
+)
+
+SUBJECT_NEW_COMMENT_DEFAULT = _('You were mentioned in a comment')
+
+TEMPLATE_MENTION_COMMENT_DEFAULT = _(
+    "<h2 style=\"font-weight: 500; font-size: 20px\">Company: <strong><span style=\"text-align: center;\"> __company_title__ </span></strong></h2>"
+    "<p style=\"font-size: 18px\">Hi, </p>"
+    "<blockquote style=\"font-style: italic;\"><span class=\"params-data\" data-code=\"_mention__comment_msg\"></span></blockquote>"
+    "<p><a href=\"_mention__links\" style=\"text-decoration:none;color:#fff;width: 150px;display: inline-block;background: #007D88;padding: 6px;text-transform: uppercase;letter-spacing: .95px; text-align:center;\">View document</a></p>"
+)
+
+SUBJECT_NEW_TASK_DEFAULT = _('🔔 Notification: You have new to do tasks')
+
+TEMPLATE_NEW_TASKS_DEFAULT = _(
+    "<h2 style=\"font-weight: 500; font-size: 20px\">Company: <strong><span style=\"text-align: center;\"> __company_title__ </span></strong></h2>"
+    "<p style=\"font-size: 18px\">Hi, <span class=\"params-data\" data-code=\"_task__employee_inherit\"></span></p>"
+    "<p>You have a new task! <strong style=\"font-style: italic;\"><span class=\"params-data\" data-code=\"_task__assigner\"></span></strong> has just sent you a task that needs to be processed. Please check and start working on it.</p>"
+    "<p><a href=\"_task__links\" style=\"text-decoration:none;color:#fff;width: 150px;display: inline-block;background: #007D88;padding: 6px;text-transform: uppercase;letter-spacing: .95px; text-align:center;\">View document</a></p>"
 )
