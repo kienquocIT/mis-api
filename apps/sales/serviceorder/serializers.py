0d773238dd7c62d05965a88e5dcaa2789a644840
Windows-key <giacanemail@gmail.com>
2025-09-03 15:44:52 +0700

    [feature/ms-244]: fix pylint

--- apps/sales/serviceorder/serializers.py
+++ apps/sales/serviceorder/serializers.py
@@ -20,7 +20,7 @@ __all__ = [
 ]
 
 
-# ================================ COMMON FUNC =====================================
+# COMMON FUNCTION
 class ServiceOrderCommonFunc:
     @staticmethod
     def create_attachment(doc_id, attachment_result):
@@ -37,91 +37,23 @@ class ServiceOrderCommonFunc:
         return True
 
     @staticmethod
-    def assign_packages_to_containers(shipment, packages_data):
-        # containers = ServiceOrderContainer.objects.filter(shipment=shipment)
-
-        # container_list = list(containers)
-        # package_list = []
-
-        for index, package_data in enumerate(packages_data):
-            break
-            # target_container = container_list[i % len(container_list)]
-
-    @staticmethod
-    def create_service_order_packages(shipment, container, packages_data):
-        package_list = []
-
-        for item in packages_data:
-            package = ServiceOrderPackage(
-                shipment_id=str(shipment.id),
-                container_reference_id=str(container.id),
-                order=item.get('order', 1),
-                package_type_id=item.get('package_type')
-            )
-            package_list.append(package)
-
-        # bulk create packages
-        ServiceOrderPackage.objects.bulk_create(package_list)
-        return True
-
-    @staticmethod
-    def create_service_order_containers(shipment, containers_data):
-        container_list = []
-        all_packages_data = []
-        for item in containers_data:
-            packages_data = item.pop('packages', [])
-
-            container = ServiceOrderContainer(
-                shipment_id=str(shipment.id),
-                order=item.get('order', 1),
-                container_type_id=item.get('container_type'),
-            )
-            container_list.append(container)
-            all_packages_data.append(packages_data)
-
-        # bulk create containers
-        created_containers = ServiceOrderContainer.objects.bulk_create(container_list)
-
-        # create packages for each container
-        for index, container in enumerate(created_containers):
-            packages_data = all_packages_data[index]
-            if packages_data:
-                ServiceOrderCommonFunc.create_service_order_packages(shipment, container, packages_data)
-        return True
-
-    @staticmethod
-    def create_service_order_shipment(service_order, shipment_data):
-        containers_data = shipment_data.pop('containers', [])
-        packages_data = shipment_data.pop('packages', [])
-
-        shipment = ServiceOrderShipment.objects.create(
-            service_order_id=str(service_order.id),
-            reference_number=shipment_data.get('reference_number', ''),
-            weight=shipment_data.get('weight', 0),
-            dimension=shipment_data.get('dimension', 0),
-            description=shipment_data.get('description', ''),
-            is_container=shipment_data.get('is_container', True),
-            company=service_order.company,
-            tenant=service_order.tenant
-        )
-
-        if containers_data:
-            ServiceOrderCommonFunc.create_service_order_containers(shipment, containers_data)
-        if packages_data:
-            ServiceOrderCommonFunc.assign_packages_to_containers(shipment, packages_data)
-
-        return True
+    def create_shipment(service_order, shipment_data):
+        # new_shipment = []
+        # for shipment in shipment_data:
+        #
+        # pass
+        pass
 
 
-# ================================ SHIPMENT =====================================
+# SHIPMENT
 class ServiceOderShipmentSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=100)
     reference_number = serializers.CharField(max_length=100, required=False, allow_null=True)
     weight = serializers.FloatField(default=0)
     dimension = serializers.FloatField(default=0)
     description = serializers.CharField(required=True, allow_blank=True)
-    is_container = serializers.BooleanField(default=True)
     reference_container = serializers.CharField(max_length=100, required=False, allow_null=True)
+    is_container = serializers.BooleanField(default=True)
 
     class Meta:
         model = ServiceOrderShipment
@@ -163,7 +95,7 @@ class ServiceOrderCreateSerializer(AbstractCreateSerializerModel):
     start_date = serializers.DateField()
     end_date = serializers.DateField()
 
-    # shipments = ServiceOderShipmentSerializer(many=True)
+    shipment = ServiceOderShipmentSerializer(many=True)
     # attachment = serializers.ListSerializer(child=serializers.CharField(), required=False)
 
     # def validate_attachment(self, value):
@@ -198,10 +130,10 @@ class ServiceOrderCreateSerializer(AbstractCreateSerializerModel):
     @decorator_run_workflow
     def create(self, validated_data):
         with transaction.atomic():
-            shipments = validated_data.pop('shipments', [])
+            shipment = validated_data.pop('shipment', [])
             attachment = validated_data.pop('attachment', [])
             service_order = ServiceOrder.objects.create(**validated_data)
-            # ServiceOrderCommonFunc.create_service_order_shipments(service_order.id, shipments)
+            ServiceOrderCommonFunc.create_service_order_shipments(service_order.id, shipment)
             # ServiceOrderCommonFunc.create_attachment(service_order.id, attachment)
             # SerializerCommonHandle.handle_attach_file(
             #     relate_app=Application.objects.filter(id="36f25733-a6e7-43ea-b710-38e2052f0f6d").first(),
@@ -218,7 +150,7 @@ class ServiceOrderCreateSerializer(AbstractCreateSerializerModel):
             'customer',
             'start_date',
             'end_date',
-            # 'shipments',
+            'shipment',
             # 'attachment'
         )
 
