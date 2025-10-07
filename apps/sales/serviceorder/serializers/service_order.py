413dbdaeb1c7903fcea0ed8f922eb6e3177216f3
namviet <vietnn@mtsolution.com>
2025-10-07 17:32:23 +0700

    [feature/ms-008] pylint

--- apps/sales/serviceorder/serializers/service_order.py
+++ apps/sales/serviceorder/serializers/service_order.py
@@ -270,7 +270,8 @@ class ServiceOrderDetailSerializer(AbstractDetailSerializerModel):
             'attributes_total_cost': service_detail.attributes_total_cost,
             'duration_id': service_detail.duration_id if service_detail.duration else None,
             'duration_unit_data': service_detail.duration_unit_data,
-            'duration': service_detail.duration_value if hasattr(service_detail, 'duration_value') and service_detail.duration else 1,
+            'duration': service_detail.duration_value if hasattr(service_detail,
+                                                                 'duration_value') and service_detail.duration else 1,
             'has_attributes': bool(service_detail.selected_attributes and service_detail.selected_attributes != {}),
         } for service_detail in obj.service_details.all()]
 
