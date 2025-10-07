6751a234c73b7159f381215a3e2efa2086284ee3
haind <haind@mtsolution.com.vn>
2025-10-07 16:24:00 +0700

    [ft/440]: update Inventory report

--- apps/sales/report/utils/log_for_goods_transfer.py
+++ apps/sales/report/utils/log_for_goods_transfer.py
@@ -94,11 +94,11 @@ class IRForGoodsTransferHandler:
                 if item.product.valuation_method == 2:
                     for serial_id in item.sn_data:
                         serial_obj = ProductWareHouseSerial.objects.filter(id=serial_id).first()
-                        specific_value = ProductSpecificIdentificationSerial.get_specific_value(
-                            product=item.product,
-                            serial_number=serial_obj.serial_number
-                        )
                         if serial_obj:
+                            specific_value = ProductSpecificIdentificationSerial.get_specific_value(
+                                product=item.product,
+                                serial_number=serial_obj.serial_number
+                            )
                             doc_data_out.append({
                                 'sale_order': item.sale_order,
                                 'product': item.product,
