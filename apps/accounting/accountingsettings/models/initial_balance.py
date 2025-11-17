eecc9aff58bc777cfd4a61b6762d5eac94d10094
ndh34 <nguyenduongghai07@gmail.com>
2025-11-17 15:04:29 +0700

    [feature/haind-accounting]: update model IB

--- apps/accounting/accountingsettings/models/initial_balance.py
+++ apps/accounting/accountingsettings/models/initial_balance.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from apps.shared import MasterDataAbstractModel, SimpleAbstractModel, DataAbstractModel
+from apps.shared import SimpleAbstractModel, DataAbstractModel
 
 
 __all__ = [
