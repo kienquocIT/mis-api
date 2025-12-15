3edd4d3d8569ca9414b620468ac5d6fa91704d89
ndh34 <nguyenduongghai07@gmail.com>
2025-12-15 15:20:01 +0700

    [feature/accounting]: update Employee List Export + JE + Recon

--- apps/sales/report/utils/inventory_log.py
+++ apps/sales/report/utils/inventory_log.py
@@ -114,9 +114,9 @@ class ReportInvCommonFunc:
             'opening_balance_quantity': last_sub_item.ending_balance_quantity,
             'opening_balance_cost': last_sub_item.ending_balance_cost,
             'opening_balance_value': last_sub_item.ending_balance_value,
-            'periodic_ending_balance_quantity': last_sub_item.periodic_ending_balance_quantity,
-            'periodic_ending_balance_cost': last_sub_item.periodic_ending_balance_cost,
-            'periodic_ending_balance_value': last_sub_item.periodic_ending_balance_value
+            'ending_balance_quantity': last_sub_item.ending_balance_quantity,
+            'ending_balance_cost': last_sub_item.ending_balance_cost,
+            'ending_balance_value': last_sub_item.ending_balance_value
         }
         rp_inv_cost = ReportInventoryCost(**item_parsed)
         bulk_info.append(rp_inv_cost)
